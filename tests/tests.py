ed70242947a75cd0e70270f155b4eeedf5ee51c8
Dominique Makowski <dom.mak19@gmail.com>
2019-10-29 18:14:14 +0800

    revert

--- tests/tests.py
+++ tests/tests.py
@@ -13,13 +13,13 @@ def response():
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#    import requests
+#    return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+#    from bs4 import BeautifulSoup
+#    assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
