465848e386f6925729ec5040af9121f42747d66f
Dominique Makowski <dom.mak19@gmail.com>
2019-10-30 10:50:41 +0800

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,6 @@
-import unittest
-import doctest
-import numpy as np
-import pandas as pd
-import neurokit as nk
+import pytest
+import neurokit2 as nk
 
-from test_ecg import TestEcg
-from test_signal import TestComplexity
-from test_miscellaneous import TestMiscellaneous
-from test_statistics import TestStatistics
 
 if __name__ == '__main__':
     unittest.main()
