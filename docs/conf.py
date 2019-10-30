7bb5a61a54fa4e29f235a7cd265fedac53dfbe60
Dominique Makowski <dom.mak19@gmail.com>
2019-10-30 12:23:00 +0800

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -90,6 +90,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_favicon = "img/icon.ico"
+html_logo = "img/neurokit.png"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
