e5a1ade0482bb9e16925e058422d621f3556661e
Dominique Makowski <dom.mak19@gmail.com>
2019-11-26 10:25:12 +0800

    Update tests_events.py

--- tests/tests_events.py
+++ tests/tests_events.py
@@ -1,24 +1,24 @@
-#import numpy as np
-#import pandas as pd
-#import neurokit2 as nk
-#
-## =============================================================================
-## Events
-## =============================================================================
-#
-#
-#
-#def test_events_find():
-#
-#    signal = np.cos(np.linspace(start=0, stop=20, num=1000))
-#    events = nk.events_find(signal)
-#    assert list(events["Onset"]) == [0, 236, 550, 864]
-#
-#
-#
-#def test_plot_events_in_signal():
-#
-#    signal = np.cos(np.linspace(start=0, stop=20, num=1000))
-#    events = nk.events_find(signal)
-#    data = nk.plot_events_in_signal(signal, events, show=False)
-#    assert len(data['Event_Onset']) == 1000
+import numpy as np
+import pandas as pd
+import neurokit2 as nk
+
+# =============================================================================
+# Events
+# =============================================================================
+
+
+
+def test_events_find():
+
+    signal = np.cos(np.linspace(start=0, stop=20, num=1000))
+    events = nk.events_find(signal)
+    assert list(events["Onset"]) == [0, 236, 550, 864]
+
+
+
+def test_plot_events_in_signal():
+
+    signal = np.cos(np.linspace(start=0, stop=20, num=1000))
+    events = nk.events_find(signal)
+    data = nk.plot_events_in_signal(signal, events, show=False)
+    assert len(data['Event_Onset']) == 1000
