09c3d925a6818411851673c431cd1ed60fd18525
Dominique Makowski <dom.mak19@gmail.com>
2019-12-05 16:52:02 +0800

    fix resampling

--- neurokit2/signal/signal_resample.py
+++ neurokit2/signal/signal_resample.py
@@ -36,26 +36,37 @@ def signal_resample(signal, desired_length=None, sampling_rate=None, desired_sam
     >>> import pandas as pd
     >>> import neurokit2 as nk
     >>>
-    >>> # Downsample and resample back to compare with original (and do time benchmarking)
-    >>> # Downsampling
     >>> signal = np.cos(np.linspace(start=0, stop=20, num=100))
-    >>> %timeit downsampled_interpolation = nk.signal_resample(signal, method="interpolation", sampling_rate=1000, desired_sampling_rate=500)
-    >>> %timeit downsampled_interpolation2 = nk.signal_resample(signal, method="interpolation2", sampling_rate=1000, desired_sampling_rate=500)
-    >>> %timeit downsampled_fft = nk.signal_resample(signal, method="FFT", sampling_rate=1000, desired_sampling_rate=500)
-    >>> %timeit downsampled_poly = nk.signal_resample(signal, method="poly", sampling_rate=1000, desired_sampling_rate=500)
     >>>
-    >>> # Upsampling
-    >>> %timeit upsampled_interpolation = nk.signal_resample(downsampled_interpolation, method="interpolation", sampling_rate=500, desired_sampling_rate=1000)
-    >>> %timeit upsampled_interpolation2 = nk.signal_resample(downsampled_interpolation2, method="interpolation2", sampling_rate=500, desired_sampling_rate=1000)
-    >>> %timeit upsampled_fft = nk.signal_resample(downsampled_fft, method="FFT", sampling_rate=500, desired_sampling_rate=1000)
-    >>> %timeit upsampled_poly = nk.signal_resample(downsampled_poly, method="poly", sampling_rate=500, desired_sampling_rate=1000)
+    >>> # Downsample
     >>>
-    >>> # Check
+    >>> downsampled_interpolation = nk.signal_resample(signal, method="interpolation", sampling_rate=1000, desired_sampling_rate=500)
+    >>> downsampled_fft = nk.signal_resample(signal, method="FFT", sampling_rate=1000, desired_sampling_rate=500)
+    >>> downsampled_poly = nk.signal_resample(signal, method="poly", sampling_rate=1000, desired_sampling_rate=500)
+    >>> downsampled_numpy = nk.signal_resample(signal, method="numpy", sampling_rate=1000, desired_sampling_rate=500)
+    >>> downsampled_pandas = nk.signal_resample(signal, method="pandas", sampling_rate=1000, desired_sampling_rate=500)
+    >>>
+    >>> # Upsample
+    >>> upsampled_interpolation = nk.signal_resample(downsampled_interpolation, method="interpolation", sampling_rate=500, desired_sampling_rate=1000)
+    >>> upsampled_fft = nk.signal_resample(downsampled_fft, method="FFT", sampling_rate=500, desired_sampling_rate=1000)
+    >>> upsampled_poly = nk.signal_resample(downsampled_poly, method="poly", sampling_rate=500, desired_sampling_rate=1000)
+    >>> upsampled_numpy = nk.signal_resample(downsampled_numpy, method="numpy", sampling_rate=500, desired_sampling_rate=1000)
+    >>> upsampled_pandas = nk.signal_resample(downsampled_pandas, method="pandas", sampling_rate=500, desired_sampling_rate=1000)
+    >>>
+    >>> # Compare with original
     >>> pd.DataFrame({"Original": signal,
                       "Interpolation": upsampled_interpolation,
-                      "Interpolation2": upsampled_interpolation2,
                       "FFT": upsampled_fft,
-                      "Poly": upsampled_poly}).plot(style='.-')
+                      "Poly": upsampled_poly,
+                      "Numpy": upsampled_numpy,
+                      "Pandas": upsampled_pandas}).plot(style='.-')
+    >>>
+    >>> # Timing benchmarks
+    >>> %timeit nk.signal_resample(signal, method="interpolation", sampling_rate=1000, desired_sampling_rate=500)
+    >>> %timeit nk.signal_resample(signal, method="FFT", sampling_rate=1000, desired_sampling_rate=500)
+    >>> %timeit nk.signal_resample(signal, method="poly", sampling_rate=1000, desired_sampling_rate=500)
+    >>> %timeit nk.signal_resample(signal, method="numpy", sampling_rate=1000, desired_sampling_rate=500)
+    >>> %timeit nk.signal_resample(signal, method="pandas", sampling_rate=1000, desired_sampling_rate=500)
     """
     if desired_length is None:
         desired_length = round(len(signal) * desired_sampling_rate / sampling_rate)
@@ -115,7 +126,16 @@ def _resample_pandas(signal, desired_length):
     resampled_signal = pd.Series(signal, index=index)
 
     # Create resampling factor
-    resampling_factor = str(round(1/(desired_length / len(signal)))) + "L"
+    resampling_factor = str(1/(desired_length / len(signal))) + "L"
 
-    resampled_signal = resampled_signal.resample(resampling_factor).mean()
-    return(resampled_signal.values)
+    # Resample
+    resampled_signal = resampled_signal.resample(resampling_factor).bfill().values
+
+    # Adjust extremities
+    diff = len(resampled_signal) - desired_length
+    if diff < 0:
+        resampled_signal = np.concatenate([resampled_signal, np.full(abs(diff), resampled_signal[-1])])
+    elif diff > 0:
+        resampled_signal = resampled_signal[0:desired_length]
+
+    return(resampled_signal)
