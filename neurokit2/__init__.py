107d1a87dd76fdcd1d9d2d0f952a66f5d9b58de5
Dominique Makowski <dom.mak19@gmail.com>
2019-12-05 22:17:14 +0800

    tests

--- neurokit2/__init__.py
+++ neurokit2/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """NeuroKit development team"""
 __email__ = 'dom.makowski@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .signal import *
 from .events import *
--- tests/tests.py
+++ tests/tests.py
@@ -10,19 +10,3 @@ if __name__ == '__main__':
     doctest.testmod()
     pytest.main()
 
-# from .tests_events import *
-
-# =============================================================================
-# Signal
-# =============================================================================
-
-
-
-def test_signal_binarize():
-
-    signal = np.cos(np.linspace(start=0, stop=20, num=1000))
-    binary = nk.signal_binarize(signal)
-    assert len(binary) == 1000
-
-    binary = nk.signal_binarize(list(signal))
-    assert len(binary) == 1000
