f77a2829dd95950cb517c1b93005e4e3f7f8cfa3
Dominique Makowski <dom.mak19@gmail.com>
2019-12-08 22:02:29 +0800

    Update tests_stats.py

--- tests/tests_stats.py
+++ tests/tests_stats.py
@@ -13,5 +13,13 @@ def test_standardize():
     rez = np.sum(nk.standardize([1, 1, 5, 2, 1]))
     assert np.allclose(rez, 0, atol=0.0001)
 
+    rez = np.sum(nk.standardize(np.array([1, 1, 5, 2, 1])))
+    assert np.allclose(rez, 0, atol=0.0001)
+
+    rez = np.sum(nk.standardize(pd.Series([1, 1, 5, 2, 1])))
+    assert np.allclose(rez, 0, atol=0.0001)
+
     rez = np.sum(nk.standardize([1, 1, 5, 2, 1, 5, 1, 7], robust=True))
     assert np.allclose(rez, 14.8387, atol=0.001)
+
+
