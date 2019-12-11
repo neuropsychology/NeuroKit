3bdd109fd9f7978f8c7a97da69cda1651d2c8533
Dominique Makowski <dom.mak19@gmail.com>
2019-12-11 14:07:13 +0800

    add detrending

--- neurokit2/signal/signal_detrend.py
+++ neurokit2/signal/signal_detrend.py
@@ -2,9 +2,10 @@
 import numpy as np
 import pandas as pd
 
-from scipy.signal import detrend
+import sklearn.linear_model
 
-def signal_detrend(signal):
+
+def signal_detrend(signal, method="linear", degree=1):
     """
     Examples
     --------
@@ -12,10 +13,21 @@ def signal_detrend(signal):
     >>> import pandas as pd
     >>> import neurokit2 as nk
     >>>
-    >>> signal = np.cos(np.linspace(start=0, stop=10, num=1000)) # Low freq
-    >>> signal += np.cos(np.linspace(start=0, stop=100, num=1000)) # High freq
-    >>> detrended = signal_detrend(signal, highcut=10)
-    >>> pd.DataFrame({"Raw": signal, "Filtered": filtered}).plot()
+    >>> signal = np.cos(np.linspace(start=0, stop=10, num=1000))  # Low freq
+    >>> signal += np.cos(np.linspace(start=0, stop=100, num=1000))  # High freq
+    >>> signal += 3  # Add baseline
+    >>>
+    >>> detrended = signal_detrend(signal, degree=1)
+    >>> pd.DataFrame({"Raw": signal,
+                      "Baseline_Detrend": signal_detrend(signal, degree=0),
+                      "Linear_Detrend": signal_detrend(signal, degree=1),
+                      "Quadratic_Detrend": signal_detrend(signal, degree=2),
+                      "Cubic_Detrend": signal_detrend(signal, degree=3)}).plot()
     """
-
-    return(signal)
+    x_axis = np.linspace(0, 100, num=len(signal))
+    # Generating weights and model for polynomial function with degree =2
+    model = np.poly1d(np.polyfit(x_axis, signal, degree))
+    trend = model(x_axis)
+    # detrend
+    detrended = np.array(signal) - trend
+    return(detrended)
