1892d09ca41d0d824908ec6627e81998c12999d1
Dominique Makowski <dom.mak19@gmail.com>
2019-12-13 16:54:31 +0800

    comment out

--- neurokit2/rsp/rsp_simulate.py
+++ neurokit2/rsp/rsp_simulate.py
@@ -2,8 +2,8 @@
 import pandas as pd
 import numpy as np
 
-def rsp_simulate(cycles = 10, frequency = 1, amplitude = 1):
 
+def rsp_simulate(duration=10, length=None, sampling_rate=1000, breathing_rate = 1, amplitude = 1):
     """Simulate a respiratory signal
 
     Generate an artificial (synthetic) respiratory signal of a given frequency, amplitude, and number of cycles. It uses a trigonometric sine wave that roughly approximates a single respiratory cycle.
@@ -12,9 +12,9 @@ def rsp_simulate(cycles = 10, frequency = 1, amplitude = 1):
     ----------
     cycles : int
         Desired total number of cycles, or number of breaths taken during the given duration.
-    frequency: int
+    frequency : int
         Desired number of cycles in one second (in Hz).
-    amplitude: int
+    amplitude : int
         The maximum displacement of each waveform, or breath, from the equilibrium or rest state (in millivolts).
 
     Returns
@@ -25,27 +25,30 @@ def rsp_simulate(cycles = 10, frequency = 1, amplitude = 1):
     ----------
     >>> import pandas as pd
     >>> import numpy as np
-    >>> import matplotlib.pyplot as plt
+    >>> import neurokit2 as nk
     >>>
-    >>> t, rsp = rsp_simulate(cycles = 15, frequency = 1, amplitude = 1)
-    >>> plt.plot(t, rsp)
+    >>> rsp = nk.rsp_simulate(cycles = 15, frequency = 1, amplitude = 1)
+    >>> nk.signal_plot(rsp)
 
     See Also
     --------
     rsp_clean, rsp_findpeaks, rsp_rate, rsp_process, rsp_plot
 """
-    # Duration of one complete oscillation
-    t_1_cycle = 2*(np.pi/frequency)
-
-    # Duration of the entire respiratory signal
-    duration = t_1_cycle * cycles
-
-    # Generate 1000 values along the length of the duration
-    t = np.linspace(0, duration, 1000)
-
-    # Compute the value of sine computed by the following trigonometric function
-    rsp = amplitude*np.sin(frequency*t)
-
-    return(t, rsp)
+#    # Duration of one complete oscillation
+#    time_cycle = 2*(np.pi/frequency)
+#
+#    # Duration of the entire respiratory signal (in seconds)
+#    duration = time_cycle * cycles
+#
+#    if length is None:
+#        length = duration * sampling_rate
+#
+#    # Generate 1000 values along the length of the duration
+#    t = np.linspace(0, duration, int(length))
+#
+#    # Compute the value of sine computed by the following trigonometric function
+#    rsp = amplitude*np.sin(frequency*t)
+#
+#    return(rsp)
 
 
