7b11ff2e792f58db51ef9106620c89a8b8026c3d
Zen <zen-juen@users.noreply.github.com>
2019-12-15 00:02:41 +0800

    update

--- neurokit2/rsp/rsp_simulate.py
+++ neurokit2/rsp/rsp_simulate.py
@@ -2,24 +2,27 @@
 import pandas as pd
 import numpy as np
 
-
-def rsp_simulate(duration=10, length=None, sampling_rate=1000, breathing_rate = 1, amplitude = 1):
+def rsp_simulate(duration=10, length=None, sampling_rate=1000, breathing_rate = 0.25, amplitude = 1):
     """Simulate a respiratory signal
 
     Generate an artificial (synthetic) respiratory signal of a given frequency, amplitude, and number of cycles. It uses a trigonometric sine wave that roughly approximates a single respiratory cycle.
 
     Parameters
     ----------
-    cycles : int
-        Desired total number of cycles, or number of breaths taken during the given duration.
-    frequency : int
-        Desired number of cycles in one second (in Hz).
+    duration : int
+        Desired length of duration (s).
+    sampling_rate, length : int
+        The desired sampling rate (in Hz, i.e., samples/second) or the desired length of the signal (in samples).
+    breathing_rate : float
+        Desired number of cycles, or number of breaths, in one second (in Hz).
     amplitude : int
         The maximum displacement of each waveform, or breath, from the equilibrium or rest state (in millivolts).
 
+
     Returns
     ----------
-    Returns two arrays. The first array, 't', contains float values along the length of the duration. The second array, 'rsp', contains float values of the amplitude that exactly correspond to time 't'.
+    array
+        Vector containing the respiratory signal.
 
     Examples
     ----------
@@ -27,28 +30,21 @@ def rsp_simulate(duration=10, length=None, sampling_rate=1000, breathing_rate = 
     >>> import numpy as np
     >>> import neurokit2 as nk
     >>>
-    >>> rsp = nk.rsp_simulate(cycles = 15, frequency = 1, amplitude = 1)
+    >>> rsp = nk.rsp_simulate(duration = 10, length = None, sampling_rate = 1000, breathing_rate = 0.5, amplitude = 2)
     >>> nk.signal_plot(rsp)
 
     See Also
     --------
     rsp_clean, rsp_findpeaks, rsp_rate, rsp_process, rsp_plot
 """
-#    # Duration of one complete oscillation
-#    time_cycle = 2*(np.pi/frequency)
-#
-#    # Duration of the entire respiratory signal (in seconds)
-#    duration = time_cycle * cycles
-#
-#    if length is None:
-#        length = duration * sampling_rate
-#
-#    # Generate 1000 values along the length of the duration
-#    t = np.linspace(0, duration, int(length))
-#
-#    # Compute the value of sine computed by the following trigonometric function
-#    rsp = amplitude*np.sin(frequency*t)
-#
-#    return(rsp)
+    # Generate number of samples automatically if length is unspecified
+    if length is None:
+        length = duration * sampling_rate
 
+    # Generate 1000 values along the length of the duration
+    t = np.linspace(0, duration, int(length))
 
+    # Compute the value of sine computed by the following trigonometric function
+    rsp = amplitude*np.sin(2*np.pi*breathing_rate*t)
+
+    return(rsp)
