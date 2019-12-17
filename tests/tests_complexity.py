fe3deb87b776558bf41b80155f54240d1d96375c
Dominique Makowski <dom.mak19@gmail.com>
2019-12-17 09:54:49 +0800

    external validation for complexity

--- tests/tests_complexity.py
+++ tests/tests_complexity.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import neurokit2 as nk
 
+from pyentrp import entropy as pyentrp
 
 # =============================================================================
 # Stats
@@ -14,5 +15,5 @@ def test_complexity():
     signal = np.cos(np.linspace(start=0, stop=30, num=len(eeg)))*70
 
     assert np.allclose(nk.complexity_shannon(eeg), 7.566810239706894, atol=0.0001)
-
+    assert pyentrp.shannon_entropy(eeg) == nk.complexity_shannon(eeg)
 
