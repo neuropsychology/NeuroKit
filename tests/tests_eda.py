744ffabd743cc713cd68696b6a62a05a70823185
Dominique Makowski <dom.mak19@gmail.com>
2019-12-22 09:33:47 +0800

    remove testing of internals

--- tests/tests_eda.py
+++ tests/tests_eda.py
@@ -16,8 +16,3 @@ def test_eda_simulate():
 #   pd.DataFrame({"EDA1": eda1, "EDA2": eda2}).plot()
 
     assert len(nk.signal_findpeaks(eda2, height_min=0.6)[0]) > len(nk.signal_findpeaks(eda1, height_min=0.6)[0])
-
-    eda3 = nk._eda_simulate_canonical(sampling_rate=1000, length=None, time_peak=10)
-    eda4 = nk._eda_simulate_canonical(sampling_rate=1000, length=None, time_peak=3)
-#   pd.DataFrame({"EDA3": eda3, "EDA4": eda4}).plot()
-    assert np.argmax(eda4) < np.argmax(eda3)
