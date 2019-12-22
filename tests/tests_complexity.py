8ec74e6302f9b6d8367fee26a5614922601f076e
Dominique Makowski <dom.mak19@gmail.com>
2019-12-22 23:14:59 +0800

    manifest

--- tests/tests_complexity.py
+++ tests/tests_complexity.py
@@ -4,10 +4,12 @@ import neurokit2 as nk
 
 import nolds
 
-#from packages import pyrem
-#from packages import pyeeg
 from pyentrp import entropy as pyentrp
 
+# Local packages (copied in the test folder)
+#from packages import pyrem
+#from packages import pyeeg
+#import entropy
 
 # =============================================================================
 # Complexity
@@ -25,7 +27,9 @@ def test_complexity():
     # Approximate
     assert np.allclose(nk.entropy_approximate(signal), 0.17364897858477146, atol=0.000001)
     assert np.allclose(nk.entropy_approximate(np.array([85, 80, 89] * 17)), 1.0996541105257052e-05, atol=0.000001)
-#    assert nk.entropy_approximate(signal, 2, 0.2) == pyeeg.ap_entropy(signal, 2, 0.2)
+#    assert nk.entropy_approximate(signal, 2, 0.2*np.std(signal, ddof=1)) == entropy.app_entropy(signal, 2)
+#    assert nk.entropy_approximate(signal, 2, 0.2*np.std(signal, ddof=1)) == pyeeg.ap_entropy(signal, 0.2*np.std(signal, ddof=1))
+
 
     # Sample
     assert np.allclose(nk.entropy_sample(signal, order=2, r=0.2*np.std(signal)),
