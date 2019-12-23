1911f777a107755375458fd8039b62177b6e33dd
Dominique Makowski <dom.mak19@gmail.com>
2019-12-23 10:09:09 +0800

    remove local pkgs

--- tests/packages/entropy/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import EntroPy objects
-from .utils import *
-from .entropy import *
-from .fractal import *
-
-__version__ = "0.1.1"
--- tests/tests_complexity.py
+++ tests/tests_complexity.py
@@ -23,14 +23,17 @@ def test_complexity():
 
     signal = np.cos(np.linspace(start=0, stop=30, num=100))
 
+
     # Shannon
     assert np.allclose(nk.entropy_shannon(signal), 6.6438561897747395, atol=0.0000001)
     assert nk.entropy_shannon(signal) == pyentrp.shannon_entropy(signal)
+
 
     # Approximate
     assert np.allclose(nk.entropy_approximate(signal), 0.17364897858477146, atol=0.000001)
     assert np.allclose(nk.entropy_approximate(np.array([85, 80, 89] * 17)), 1.0996541105257052e-05, atol=0.000001)
     assert nk.entropy_approximate(signal, 2, 0.2*np.std(signal, ddof=1)) == entropy_app_entropy(signal, 2)
+
     assert nk.entropy_approximate(signal, 2, 0.2*np.std(signal, ddof=1)) != pyeeg_ap_entropy(signal, 2, 0.2*np.std(signal, ddof=1))
 
 
@@ -38,9 +41,12 @@ def test_complexity():
     assert np.allclose(nk.entropy_sample(signal, order=2, r=0.2*np.std(signal)),
                        nolds.sampen(signal, emb_dim=2, tolerance=0.2*np.std(signal)), atol=0.000001)
     assert nk.entropy_sample(signal, 2, 0.2*np.std(signal, ddof=1)) == entropy_sample_entropy(signal, 2)
+    assert nk.entropy_sample(signal, 2, 0.2) == nolds.sampen(signal, 2, 0.2)
+
+    assert nk.entropy_sample(signal, 2, 0.2) != pyentrp.sample_entropy(signal, 2, 0.2)[1]
 #    assert nk.entropy_sample(signal, 2, 0.2) == pyeeg.samp_entropy(signal, 2, 0.2)
 #    assert nk.entropy_sample(signal, 2, 0.2) == pyrem.samp_entropy(signal, 2, 0.2, relative_r=False)
-#    pyentrp.sample_entropy(signal, 2, 0.2)  # Gives something different
+
 
     # Fuzzy
     assert np.allclose(nk.entropy_fuzzy(signal), 0.5216395432372958, atol=0.000001)
