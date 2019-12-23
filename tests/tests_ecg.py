4537c744e9d6ee07ba06ba363d1fb90329ec316b
Zen <zen-juen@users.noreply.github.com>
2019-12-23 12:34:40 +0800

    Update tests_ecg.py

--- tests/tests_ecg.py
+++ tests/tests_ecg.py
@@ -19,6 +19,6 @@ def test_ecg_simulate():
 #    pd.DataFrame({"ECG1":ecg1, "ECG2": ecg2}).hist()
     assert len(nk.signal_findpeaks(ecg1)[0]) < len(nk.signal_findpeaks(ecg2)[0])
 
-    ecg3 = ecg_simulate(duration=10, length=5000)
+    ecg3 = nk.ecg_simulate(duration=10, length=5000)
 #    pd.DataFrame({"ECG1":ecg1, "ECG3": ecg3}).plot()
     assert len(nk.signal_findpeaks(ecg1, height_min=0.6)[0]) > len(nk.signal_findpeaks(ecg3, height_min=0.6)[0])
