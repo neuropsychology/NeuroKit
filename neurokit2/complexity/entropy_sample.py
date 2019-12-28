eb6648de7e5786948112c647e5cb07582db1d701
Dominique Makowski <dom.mak19@gmail.com>
2019-12-28 10:00:24 +0800

    fixes to entropy_sample #53

--- neurokit2/complexity/entropy_sample.py
+++ neurokit2/complexity/entropy_sample.py
@@ -3,45 +3,11 @@ import pandas as pd
 import numpy as np
 
 
-from .utils_embed import _embed
 from .utils_phi import _phi
 from .utils_get_r import _get_r
+from .entropy_fuzzy import _entropy_sample
 
 
-def _entropy_sample(signal, order=2, r="default", n=1, fuzzy=False):
-    """
-    Internal function adapted from https://github.com/ixjlyons/entro-py/blob/master/entropy.py
-    """
-    # Sanity checks
-    signal = np.array(signal).astype(float)
-    N = len(signal)
-
-    phi = [0, 0]  # phi(m), phi(m+1)
-    for j in [0, 1]:
-        m = order + j
-        patterns = np.transpose(_embed(signal, m))
-
-        if fuzzy:
-            patterns -= np.mean(patterns, axis=0, keepdims=True)
-
-        count = np.zeros(N-m)
-        for i in range(N-m):
-            if m == 1:
-                sub = patterns[i]
-            else:
-                sub = patterns[:, [i]]
-            dist = np.max(np.abs(patterns - sub), axis=0)
-
-            if fuzzy:
-                sim = np.exp(-np.power(dist, n) / r)
-            else:
-                sim = dist <= r
-
-            count[i] = np.sum(sim) - 1
-
-        phi[j] = np.mean(count) / (N-m-1)
-
-    return -np.log(np.divide(phi[1], phi[0]))
 
 
 
@@ -70,13 +36,14 @@ def entropy_sample(signal, order=2, r="default"):
     >>>
     >>> signal = np.cos(np.linspace(start=0, stop=30, num=100))
     >>> nk.entropy_sample(signal[0:100])
-    0.27503095489822205
+    0.281711905465277
     """
     r = _get_r(signal, r=r)
 
     # entro-py implementation (https://github.com/ixjlyons/entro-py/blob/master/entropy.py):
-#    return _entropy_sample(signal, order=order, r=r, n=1, fuzzy=False)
+#    phi = _entropy_sample(signal, order=order, r=r, n=1, fuzzy=False)
 
     # nolds and Entropy implementation:
     phi = _phi(signal, order=order, r=r, metric='chebyshev', approximate=False)
+
     return -np.log(np.divide(phi[1], phi[0]))
