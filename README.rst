2df671d27050f9db8ce83575358ec0b376a7c22f
Dominique Makowski <dom.mak19@gmail.com>
2019-12-29 12:28:27 +0800

    eda_simulate: n_peaks -> n_scr

--- README.rst
+++ README.rst
@@ -73,7 +73,7 @@ Simulate physiological signals
     # Generate synthetic signals
     ecg = nk.ecg_simulate(duration=10, heart_rate=70)
     rsp = nk.rsp_simulate(duration=10, respiratory_rate=15)
-    eda = nk.eda_simulate(duration=10, n_peaks=3)
+    eda = nk.eda_simulate(duration=10, n_scr=3)
     emg = nk.emg_simulate(duration=10, n_bursts=2)
 
     # Visualise biosignals
--- tests/tests_eda.py
+++ tests/tests_eda.py
@@ -8,10 +8,10 @@ import neurokit2 as nk
 
 def test_eda_simulate():
 
-    eda1 = nk.eda_simulate(duration=10, length=None, n_peaks=1)
+    eda1 = nk.eda_simulate(duration=10, length=None, n_scr=1)
     assert len(nk.signal_findpeaks(eda1, height_min=0.6)[0]) == 1
 
-    eda2 = nk.eda_simulate(duration=10, length=None, n_peaks=5)
+    eda2 = nk.eda_simulate(duration=10, length=None, n_scr=5)
     assert len(nk.signal_findpeaks(eda2, height_min=0.6)[0]) == 5
 #   pd.DataFrame({"EDA1": eda1, "EDA2": eda2}).plot()
 
