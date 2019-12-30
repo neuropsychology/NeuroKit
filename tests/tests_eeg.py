28630de184c238e3faf0447694241c75c2c06e21
Pham Thanh Tam <phamttam17@gmail.com>
2019-12-30 12:06:04 +0900

    update eeg_add_channel and tests_eeg
    [np.nan] multiplies a negative number will give an empty list
    => added [np.nan]*abs(diff) for diff <0

--- tests/tests_eeg.py
+++ tests/tests_eeg.py
@@ -26,6 +26,8 @@ def test_eeg_add_channel():
     assert len(df1.columns) == 377
 
     # test if the NaN is appended properly to the added channel to account for difference in distance between two signals
+    sync_index_raw=100
+    sync_index_channel=0
     for i in df1["Added_Channel"].head(abs(sync_index_channel - sync_index_raw)):
         assert np.isnan(i)
     assert np.isfinite(df1["Added_Channel"].iloc[abs(sync_index_channel - sync_index_raw)])
@@ -45,6 +47,8 @@ def test_eeg_add_channel():
     assert len(df2.columns) == 377
 
     # test if the NaN is appended properly to the added channel to account for difference in distance between two signals + difference in length
+    sync_index_raw=0
+    sync_index_channel=100
     for i in df2["Added_Channel"].tail(abs(sync_index_channel - sync_index_raw) + (len(raw) - len(ecg2))):
         assert np.isnan(i)
     assert np.isfinite(df2["Added_Channel"].iloc[-abs(sync_index_channel - sync_index_raw) - (len(raw) - len(ecg2)) - 1])
