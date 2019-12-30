66ddd054d38ae25d60934088b9df99756faa48e3
Dominique Makowski <dom.mak19@gmail.com>
2019-12-30 09:34:30 +0800

    hotfix

--- tests/tests_ecg.py
+++ tests/tests_ecg.py
@@ -21,4 +21,4 @@ def test_ecg_simulate():
 
     ecg3 = nk.ecg_simulate(duration=10, length=5000)
 #    pd.DataFrame({"ECG1":ecg1, "ECG3": ecg3}).plot()
-    assert len(nk.signal_findpeaks(ecg1, height_min=0.6)[0]) > len(nk.signal_findpeaks(ecg3, height_min=0.6)[0])
+    assert len(nk.signal_findpeaks(ecg2, height_min=0.6)[0]) > len(nk.signal_findpeaks(ecg3, height_min=0.6)[0])
