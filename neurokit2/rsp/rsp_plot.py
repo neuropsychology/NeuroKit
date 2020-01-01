6731bab3e39b85c19badd7e45a8ddbc1e76e4440
Dominique Makowski <dom.mak19@gmail.com>
2020-01-01 19:29:01 +0800

    fixed the requirement for duration
    #58 @zen-juen changed so that it doesn't require duration but sampling_rate

--- neurokit2/rsp/rsp_plot.py
+++ neurokit2/rsp/rsp_plot.py
@@ -5,12 +5,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def rsp_plot(rsp_signal, duration=60):
+def rsp_plot(rsp_signals, sampling_rate=None):
     """Visualize respiration (RSP) data.
 
     Parameters
     ----------
-    rsp_signal : DataFrame
+    rsp_signals : DataFrame
         DataFrame obtained from `rsp_process`.
 
     Examples
@@ -19,44 +19,47 @@ def rsp_plot(rsp_signal, duration=60):
     >>> import pandas as pd
     >>> import neurokit2 as nk
     >>>
-    >>> rsp = np.cos(np.linspace(start=0, stop=50, num=10000))
-    >>> rsp_signal, info = rsp_process(rsp, sampling_rate=1000)
-    >>> nk.rsp_plot(rsp_signal)
+    >>> rsp = nk.rsp_simulate(duration=90)
+    >>> rsp_signals, info = nk.rsp_process(rsp, sampling_rate=1000)
+    >>> nk.rsp_plot(rsp_signals, sampling_rate=1000)
 
     See Also
     --------
     rsp_process
     """
-    # Append time stamp
-    t = np.linspace(0, duration, len(rsp_signal))
-    t = pd.DataFrame(t)
-    rsp_signal = pd.concat([rsp_signal, t], axis=1)
-    rsp_signal.columns = [*rsp_signal.columns[:-1], 'Time']
+    # Get x-axis
+    if sampling_rate is not None:
+        x_axis = np.linspace(0, len(rsp_signals) / sampling_rate, len(rsp_signals))
+    else:
+        x_axis = np.arange(0, len(rsp_signals))
 
     # Extract peaks and troughs
-    peaks = np.where(rsp_signal["RSP_Peaks"] == 1)[0]
-    troughs = np.where(rsp_signal["RSP_Troughs"] == 1)[0]
+    peaks = np.where(rsp_signals["RSP_Peaks"] == 1)[0]
+    troughs = np.where(rsp_signals["RSP_Troughs"] == 1)[0]
 
     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, ncols=1, sharex=True)
     plt.subplots_adjust(hspace=0.2)
     ax0.set_title("Signal and Breathing Extrema")
-    ax0.plot(rsp_signal["Time"], rsp_signal["RSP_Raw"], color='grey', label='Raw', zorder=1)
-    ax0.plot(rsp_signal["Time"], rsp_signal["RSP_Clean"], color='blue', label='Cleaned', zorder=1)
+    ax0.plot(x_axis, rsp_signals["RSP_Raw"], color='grey', label='Raw', zorder=1)
+    ax0.plot(x_axis, rsp_signals["RSP_Clean"], color='blue', label='Cleaned', zorder=1)
     ax0.set_ylabel('Amplitude (m)')
     ax0.legend(loc='upper right')
-    ax0.scatter(rsp_signal["Time"][peaks], rsp_signal["RSP_Clean"][peaks], color='red', zorder=2)
-    ax0.scatter(rsp_signal["Time"][troughs], rsp_signal["RSP_Clean"][troughs], color='orange', zorder=2)
+    ax0.scatter(x_axis[peaks], rsp_signals["RSP_Clean"][peaks], color='red', zorder=2)
+    ax0.scatter(x_axis[troughs], rsp_signals["RSP_Clean"][troughs], color='orange', zorder=2)
     ax1.set_title("Breathing Rate")
-    ax1.plot(rsp_signal["Time"], rsp_signal["RSP_Rate"], color='purple', label='Data')
-    rate_mean = [np.mean(rsp_signal["RSP_Rate"])]*len(rsp_signal["RSP_Rate"])
-    ax1.plot(rsp_signal["Time"], rate_mean, label='Mean', linestyle='--', color='purple')
+    ax1.plot(x_axis, rsp_signals["RSP_Rate"], color='purple', label='Data')
+    rate_mean = [np.mean(rsp_signals["RSP_Rate"])]*len(rsp_signals["RSP_Rate"])
+    ax1.plot(x_axis, rate_mean, label='Mean', linestyle='--', color='purple')
     ax1.set_ylabel('Breaths per minute (Bpm)')
     ax1.legend(loc='upper right')
     ax2.set_title("Breathing Amplitude")
-    ax2.plot(rsp_signal["Time"], rsp_signal["RSP_Amplitude"], color='brown', label='Data')
-    amplitude_mean = [np.mean(rsp_signal["RSP_Amplitude"])]*len(rsp_signal["RSP_Amplitude"])
-    ax2.plot(rsp_signal["Time"], amplitude_mean, label='Mean', linestyle='--', color='brown')
-    ax2.set_xlabel('Time (s)')
+    ax2.plot(x_axis, rsp_signals["RSP_Amplitude"], color='brown', label='Data')
+    amplitude_mean = [np.mean(rsp_signals["RSP_Amplitude"])]*len(rsp_signals["RSP_Amplitude"])
+    ax2.plot(x_axis, amplitude_mean, label='Mean', linestyle='--', color='brown')
+    if sampling_rate is not None:
+        ax2.set_xlabel('Time (s)')
+    else:
+        ax2.set_xlabel('Data points')
     ax2.legend(loc='upper right')
     plt.show()
     return fig
