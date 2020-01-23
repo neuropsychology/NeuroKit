b50c438e0b1aa34a5f21eb8095373b0ca3f22e83
DominiqueMakowski <dom.mak19@gmail.com>
2020-01-23 21:20:06 +0800

    codeclimate

--- neurokit2/eda/eda_peaks.py
+++ neurokit2/eda/eda_peaks.py
@@ -112,7 +112,7 @@ def _eda_peaks_getfeatures(info, eda_phasic, sampling_rate=1000, recovery_percen
     # Sanity checks -----------------------------------------------------------
 
     # Peaks (remove peaks with no onset)
-    valid_peaks =  np.logical_and(info["SCR_Peaks"] > np.nanmin(info['SCR_Onsets']),
+    valid_peaks = np.logical_and(info["SCR_Peaks"] > np.nanmin(info['SCR_Onsets']),
                                   ~np.isnan(info['SCR_Onsets']))
     peaks = info["SCR_Peaks"][valid_peaks]
 
@@ -170,5 +170,3 @@ def _eda_peaks_getfeatures(info, eda_phasic, sampling_rate=1000, recovery_percen
     info["SCR_RecoveryTime"] = recovery_time
 
     return info
-
-
