b14df79a925cc863ee7c5afaa376e014862ffbc8
DominiqueMakowski <dom.mak19@gmail.com>
2020-02-09 16:07:17 +0800

    not an extension

--- docs/conf.py
+++ docs/conf.py
@@ -51,8 +51,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx_rtd_theme',
-    'nbsphinx',
-    'sphinx-nbexamples'
+    'nbsphinx'
 ]
 
 napoleon_google_docstring = False
