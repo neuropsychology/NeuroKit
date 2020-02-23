77c5b3eb77dc4e0f1c048ea9aef98aea184a361a
Hung <hungpham2511@gmail.com>
2020-02-23 10:16:59 +0800

    decouple test with fixture

--- tests/tests_ecg_delinator.py
+++ tests/tests_ecg_delinator.py
@@ -24,17 +24,26 @@ def fixture_load_ecg_data():
         return ecg, sampling_rate
 
     ecg, sampling_rate = _get_signal('bad_500.csv', sampling_rate=500)
-    yield ecg, sampling_rate
+    rpeaks = nk.ecg_findpeaks(ecg, sampling_rate=sampling_rate, method='martinez')['ECG_R_Peaks']
+    yield dict(ecg=ecg, sampling_rate=sampling_rate, rpeaks=rpeaks)
 
 
 def test_find_T_peaks_correctly(test_data):
-    ecg_signal, sampling_rate = test_data
-    rpeaks = nk.ecg_findpeaks(ecg_signal, sampling_rate=sampling_rate, method='martinez')['ECG_R_Peaks']
-    ecg_characteristics = nk.ecg_delineator(ecg_signal, rpeaks, sampling_rate, method='dwt')
+    ecg_characteristics = nk.ecg_delineator(
+        test_data['ecg'], test_data['rpeaks'], test_data['sampling_rate'], method='dwt')
 
     np.testing.assert_allclose(ecg_characteristics['ECG_T_Peaks'][:3],
-                               [537, 1031, 1682], atol=MAX_SIGNAL_DIFF * sampling_rate)
+                               [537, 1031, 1682], atol=MAX_SIGNAL_DIFF * test_data['sampling_rate'])
 
     # plt.plot(ecg_signal)
     # plt.show()
+
+
+# def test_find_P_peaks_correctly(test_data):
+#     ecg_signal, sampling_rate = test_data
+#     rpeaks = nk.ecg_findpeaks(ecg_signal, sampling_rate=sampling_rate, method='martinez')['ECG_R_Peaks']
+#     ecg_characteristics = nk.ecg_delineator(ecg_signal, rpeaks, sampling_rate, method='dwt')
+
+#     np.testing.assert_allclose(ecg_characteristics['ECG_T_Peaks'][:3],
+#                                [537, 1031, 1682], atol=MAX_SIGNAL_DIFF * sampling_rate)
 
