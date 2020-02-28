corrected mean calculation by changing indexing

neurokit2/ecg/ecg_fixpeaks.py
@@ -6,7 +6,7 @@ import scipy.signal
 import scipy.stats
 
 
-def ecg_fixpeaks(rpeaks, sampling_rate=1000, recursive=True, show=False):
+def ecg_fixpeaks(rpeaks, sampling_rate=1000, iterative=True, show=False):
     """Correct R-peaks location based on their interval (RRi).
 
     Identify erroneous inter-beat-intervals. Lipponen & Tarvainen (2019).
@@ -19,8 +19,8 @@ def ecg_fixpeaks(rpeaks, sampling_rate=1000, recursive=True, show=False):
     sampling_rate : int
         The sampling frequency of the signal that contains the peaks (in Hz,
         i.e., samples/second).
-    recursive : bool
-        Whether or not to apply the artifact correction recursively (results
+    iterative : bool
+        Whether or not to apply the artifact correction repeatedly (results
         in superior artifact correction).
     show : bool
         Whether or not to visualize artifacts and artifact thresholds.
@@ -44,7 +44,7 @@ def ecg_fixpeaks(rpeaks, sampling_rate=1000, recursive=True, show=False):
     >>>                       random_state=41)
     >>> rpeaks_uncorrected = nk.ecg_findpeaks(ecg)
     >>> artifacts, rpeaks_corrected = nk.ecg_fixpeaks(rpeaks_uncorrected,
-    >>>                                               recursive=True,
+    >>>                                               iterative=True,
     >>>                                               show=True)
     >>> rate_corrected = nk.ecg_rate(rpeaks_uncorrected,
     >>>                              desired_length=len(ecg))
@@ -69,8 +69,8 @@ def ecg_fixpeaks(rpeaks, sampling_rate=1000, recursive=True, show=False):
     rpeaks_corrected = _fix_artifacts_lipponen2019(rpeaks, artifacts,
                                                    sampling_rate)
 
-    if recursive:
-        # Recursively apply the artifact correction until the number of
+    if iterative:
+        # Iteratively apply the artifact correction until the number of
         # artifact reaches an equilibrium (i.e., the number of artifacts
         # does not change anymore from one iteration to the next)
         n_artifacts_previous = np.inf
@@ -114,11 +114,11 @@ def _find_artifacts_lipponen2019(rpeaks, sampling_rate=1000):
     rr = np.ediff1d(rpeaks, to_begin=0) / sampling_rate
     # For subsequent analysis it is important that the first element has
     # a value in a realistic range (e.g., for median filtering).
-    rr[0] = np.mean(rr)
+    rr[0] = np.mean(rr[1:])
 
     # Compute differences of consecutive periods.
     drrs = np.ediff1d(rr, to_begin=0)
-    drrs[0] = np.mean(drrs)
+    drrs[0] = np.mean(drrs[1:])
     # Normalize by threshold.
     drrs, _ = _threshold_normalization(drrs, alpha, window_half)
 