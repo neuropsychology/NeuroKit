e5b2d34f56c7b65b953da4273f9f91364124b239
JohnDoenut <j.brammer@psych.ru.nl>
2020-03-26 18:08:23 +0100

    started parameterizing ppg_simulate()

--- neurokit2/ppg/ppg_simulate.py
+++ neurokit2/ppg/ppg_simulate.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.interpolate import interp1d, PchipInterpolator, Akima1DInterpolator
+import matplotlib.pyplot as plt
+from scipy.interpolate import Akima1DInterpolator
 from neurokit2.signal import signal_distort
 
-# Naive approximation of PPG
+# Naive phenomenological approximation of PPG.
 # The PPG wave is described with four landmarks: wave onset, location of the
 # systolic peak, location of the dicrotic notch and location of the diastolic
 # peaks. These landmarks are defined based on their x and y coordinates (in a
@@ -13,60 +13,70 @@ from neurokit2.signal import signal_distort
 # sampling rate to obtain the PPG signal.
 
 
-def ppg_simulate(heartrate=70, duration=120, sfreq=100):
+def ppg_simulate(heartrate=70, duration=120, sfreq=1000,
+                 frequency_modulation=.4, show=False, random_state=None):
 
-    # at the requested sampling rate, how long is a period at the requested heart
-    # rate and how often does that period fit into the requested duration?
+    # At the requested sampling rate, how long is a period at the requested
+    # heart-rate and how often does that period fit into the requested
+    # duration?
     period = 60 / heartrate   # in seconds
     n_period = int(np.floor(duration / period))
     periods = np.ones(n_period) * period
-    # seconds at which periods end
+    # Seconds at which waves begin.
     x_onset = np.cumsum(periods)
+    # Add respiratory sinus arrythmia (frequency modulation).
     periods, x_onset = _frequency_modulation(x_onset, periods,
                                              modulation_frequency=.05,
-                                             modulation_strength=.2)
-    y_onset = 0    # in volt Mesin et al., 2019
+                                             modulation_strength=frequency_modulation)
+    # Corresponding signal amplitudes.
+    y_onset = 0
 
+    # Seconds at which the systolic peaks occur within the waves.
     x_sys = x_onset + 0.175 * periods
+    # Corresponding signal amplitudes.
     y_sys = y_onset + 1.5    # express as percentage of y_onset
 
+    # Seconds at which the dicrotic notches occur within the waves.
     x_notch = x_onset + 0.4 * periods
+    # Corresponding signal amplitudes.
     y_notch = y_onset + y_sys * .5    # express as percentage of y_onset
 
+    # Seconds at which the diatolic peaks occur within the waves.
     x_dia = x_onset + 0.45 * periods
+    # Corresponding signal amplitudes.
     y_dia = y_onset + y_sys * .51    # express as percentage of y_onset
 
     x_all = np.concatenate((x_onset, x_sys, x_notch, x_dia))
     x_all.sort(kind="mergesort")
-    x_all = np.rint(x_all * sfreq).astype(int)
+    x_all = np.rint(x_all * sfreq).astype(int)    # convert seconds to samples
     y_all = [y_onset, y_sys, y_notch, y_dia] * n_period
 
-    # f_cubic = interp1d(x_all, y_all, kind="cubic", bounds_error=False,
-    #                    fill_value=([y_all[0]], [y_all[-1]]))
-    # f_hermite = PchipInterpolator(x_all, y_all, extrapolate=False)
-    f_akima = Akima1DInterpolator(x_all, y_all)
-
+    # Interpolate a continuous signal between the landmarks (i.e., Cartesian
+    # coordinates).
+    f = Akima1DInterpolator(x_all, y_all)
     samples = np.arange(0, duration * sfreq)
+    ppg = f(samples)
+    # Remove NANs (values outside interpolation range, i.e., after last sample).
+    ppg = ppg[~np.isnan(ppg)]
 
-    # ppg_cubic = f_cubic(samples)
-    # ppg_hermite = f_hermite(samples)
-    ppg_akima = f_akima(samples)
+    # Add motion artifacts, baseline drift, and powerline noise.
+    drift = .05
+    motion = .5
+    ppg_noisy = signal_distort(ppg, sampling_rate=sfreq,
+                               noise_amplitude=[1, .5],
+                               noise_frequency=[drift, motion],
+                               powerline_amplitude=.1, powerline_frequency=50,
+                               artifacts_amplitude=0, artifacts_frequency=100,
+                               random_state=random_state)
 
-    ppg_akima = signal_distort(ppg_akima, sampling_rate=sfreq,
-                               powerline_amplitude=.1)
+    if show:
+        fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1, sharex=True)
+        ax0.scatter(x_all, y_all, c="r")
+        ax0.plot(ppg)
+        ax1.plot(ppg_noisy)
 
+    return ppg_noisy
 
-    fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1, sharex=True)
-    # ax0.scatter(x_onset, y_onset)
-    # ax0.scatter(x_sys, y_sys)
-    # ax0.scatter(x_notch, y_notch)
-    # ax0.scatter(x_dia, y_dia)
-    ax0.scatter(x_all, y_all, c="r")
-    # ax0.plot(ppg_cubic, c="g")
-    ax0.plot(ppg_akima, c="m")
-    # ax0.plot(ppg_hermite, c="k")
-    # ax0.plot(x_periods, modulator)
-    # ax1.scatter(x_periods_modulated, amp)
 
 def _frequency_modulation(seconds, periods, modulation_frequency,
                           modulation_strength):
@@ -91,12 +101,4 @@ def _frequency_modulation(seconds, periods, modulation_frequency,
 
 
 def _amplitude_modulation():
-    pass
-
-
-def _add_noise():
-    pass
-
-
-def _add_baselinedrift():
     pass
