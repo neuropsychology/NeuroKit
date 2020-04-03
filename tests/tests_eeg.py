fd2e05f346df34de6f85cdd81a6f2875d6f120c3
Tam Pham <phamttam17@gmail.com>
2020-04-03 12:48:27 +0800

    update test_eeg

--- tests/tests_eeg.py
+++ tests/tests_eeg.py
@@ -33,9 +33,6 @@ def test_eeg_add_channel():
     assert np.isfinite(df1["Added_Channel"].iloc[abs(sync_index_channel - sync_index_raw)])
 
 
-
-
-
     # len(channel) < len(raw)
     ecg2 = nk.ecg_simulate(length=166790)
 
@@ -52,3 +49,22 @@ def test_eeg_add_channel():
     for i in df2["Added_Channel"].tail(abs(sync_index_channel - sync_index_raw) + (len(raw) - len(ecg2))):
         assert np.isnan(i)
     assert np.isfinite(df2["Added_Channel"].iloc[-abs(sync_index_channel - sync_index_raw) - (len(raw) - len(ecg2)) - 1])
+
+
+def test_mne_channel_extract():
+
+    raw = mne.io.read_raw_fif(mne.datasets.sample.data_path() + '/MEG/sample/sample_audvis_raw.fif', preload=True)
+
+    # Extract 1 channel
+    name = 'EEG 053'
+
+    raw_channel = nk.mne_channel_extract(raw, name)
+    assert raw_channel.name == name
+
+    # Extract more than 1 channel
+    name2 = ['EEG 053', 'EEG 054', 'EEG 055']
+
+    raw_channel2 = nk.mne_channel_extract(raw, name2)
+    assert len(raw_channel2.columns) == 3
+    assert all(elem in name2
+               for elem in np.array(raw_channel2.columns.values, dtype=str))
