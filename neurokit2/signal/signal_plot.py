5aa703b0372eeb6b6c97142f9e909f9b57d5a7d5
Zen <zen-juen@users.noreply.github.com>
2020-04-03 11:50:15 +0800

    fix
    #181

--- neurokit2/signal/signal_plot.py
+++ neurokit2/signal/signal_plot.py
@@ -35,7 +35,7 @@ def signal_plot(signal, sampling_rate=None, subplots=False, standardize=False):
     >>> data = pd.DataFrame({"Signal2": np.cos(np.linspace(start=0, stop=20, num=1000)),
                              "Signal3": np.sin(np.linspace(start=0, stop=20, num=1000)),
                              "Signal4": nk.signal_binarize(np.cos(np.linspace(start=0, stop=40, num=1000)))})
-    >>> nk.signal_plot(data)
+    >>> nk.signal_plot(data, subplots=True)
     >>> nk.signal_plot([signal, data], standardize=True)
     """
     # Sanitize format
@@ -70,11 +70,14 @@ def signal_plot(signal, sampling_rate=None, subplots=False, standardize=False):
     if sampling_rate is not None:
         x_axis = np.linspace(0, signal.shape[0] / sampling_rate,
                              signal.shape[0])
-        x_axis = pd.DataFrame(x_axis, columns=['Time'])
+        x_axis = pd.DataFrame(x_axis, columns=['Time (s)'])
         signal = pd.concat([signal, x_axis], axis=1)
-        signal = signal.set_index('Time')
+        signal = signal.set_index('Time (s)')
     elif sampling_rate is None:
         x_axis = np.arange(0, signal.shape[0])
+        x_axis = pd.DataFrame(x_axis, columns=['Samples'])
+        signal = pd.concat([signal, x_axis], axis=1)
+        signal = signal.set_index('Samples')
 
     # Plot accordingly
     if len(events_columns) > 0:
@@ -89,13 +92,9 @@ def signal_plot(signal, sampling_rate=None, subplots=False, standardize=False):
             plot.gca().set_xlabel('Samples')
     else:
         if standardize is True:
-            plot = nk_standardize(signal[continuous_columns]).plot(subplots=subplots)
+            plot = nk_standardize(signal[continuous_columns]).plot(subplots=subplots, sharex=True)
         else:
-            plot = signal[continuous_columns].plot(subplots=subplots)
-        if sampling_rate is not None:
-            plot.set_xlabel('Time (seconds)')
-        elif sampling_rate is None:
-            plot.set_xlabel('Samples')
+            plot = signal[continuous_columns].plot(subplots=subplots, sharex=True)
 
     # Tidy legend locations
     [plot.legend(loc=1) for plot in plt.gcf().axes]
