74e0606a37ba0325d002805513a07747176a5e17
Tam Pham <phamttam17@gmail.com>
2020-04-03 11:58:19 +0800

    update tests_data

--- tests/tests_data.py
+++ tests/tests_data.py
@@ -16,6 +16,9 @@ def test_read_acqknowledge():
     df, sampling_rate = nk.read_acqknowledge(os.path.join(path_data, "acqnowledge.acq"), sampling_rate=2000)
     assert sampling_rate == 2000
 
+    df, sampling_rate = nk.read_acqknowledge(os.path.join(path_data, "acqnowledge.acq"), sampling_rate='max)
+    assert sampling_rate == 4000
+
 def test_data():
 
     dataset="bio_eventrelated_100hz"
@@ -25,3 +28,11 @@ def test_data():
     assert data.size == 15000*4
     assert all(elem in ['ECG', 'EDA', 'Photosensor', 'RSP']
                for elem in np.array(data.columns.values, dtype=str))
+
+    dataset2="bio_eventrelated_100hz.csv"
+
+    data2 = nk.data(dataset2)
+    assert len(data.columns) == len(data2.columns)
+    assert data2.size == data.size
+    assert all(elem in np.array(data.columns.values, dtype=str)
+               for elem in np.array(data2.columns.values, dtype=str))
