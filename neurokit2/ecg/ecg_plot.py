adb9a21bd5463c25c1fab1e7f6e483997baa86c5
Zen <zen-juen@users.noreply.github.com>
2020-04-07 20:24:54 +0800

    fix

--- neurokit2/ecg/ecg_plot.py
+++ neurokit2/ecg/ecg_plot.py
@@ -36,7 +36,7 @@ def ecg_plot(ecg_signals, rpeaks=None, sampling_rate=None, show_type='default'):
     >>>
     >>> ecg = nk.ecg_simulate(duration=15, sampling_rate=1000, heart_rate=80)
     >>> signals, info = nk.ecg_process(ecg, sampling_rate=1000)
-    >>> nk.ecg_plot(signals, sampling_rate=1000, show_type='artifacts')
+    >>> ecg_plot(signals, sampling_rate=1000, show_type='default')
 
     See Also
     --------
@@ -116,8 +116,6 @@ def ecg_plot(ecg_signals, rpeaks=None, sampling_rate=None, show_type='default'):
                 line, = ax2.plot(heartbeats_pivoted[x], color=color)
                 lines.append(line)
 
-    return fig
-
     # Plot artifacts
     if show_type == 'artifacts' or show_type == 'full':
         if sampling_rate is None:
@@ -127,5 +125,7 @@ def ecg_plot(ecg_signals, rpeaks=None, sampling_rate=None, show_type='default'):
             _, rpeaks = ecg_peaks(ecg_signals["ECG_Clean"],
                                   sampling_rate=sampling_rate)
 
-        ecg_fixpeaks(rpeaks, sampling_rate=sampling_rate,
-                     iterative=True, show=True)
+        fig = ecg_fixpeaks(rpeaks, sampling_rate=sampling_rate,
+                           iterative=True, show=True)
+
+    return fig
