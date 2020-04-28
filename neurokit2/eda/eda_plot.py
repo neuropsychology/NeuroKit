665e2ed295cd74f31d7373f910f7040cbef1c82f
Zen <zen-juen@users.noreply.github.com>
2020-04-28 14:50:52 +0800

    Update eda_plot.py

--- neurokit2/eda/eda_plot.py
+++ neurokit2/eda/eda_plot.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
 import matplotlib.collections
+
+from ..misc import findclosest
 
 
 def eda_plot(eda_signals, sampling_rate=None):
