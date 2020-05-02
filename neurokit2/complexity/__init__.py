61a93edd7b30e9916345d60ee87d2db19464ca34
DominiqueMakowski <dom.mak19@gmail.com>
2020-05-02 21:12:54 +0800

    Generate time series using the Mackey-Glass equation

--- neurokit2/complexity/__init__.py
+++ neurokit2/complexity/__init__.py
@@ -2,6 +2,7 @@
 
 from .embedding import embedding
 from .embedding_delay import embedding_delay
+from .complexity_simulate import complexity_simulate
 from .entropy_shannon import entropy_shannon
 from .entropy_approximate import entropy_approximate
 from .entropy_sample import entropy_sample
