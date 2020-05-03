a3910525001ff76ae71264d5f0e406a0794d2ddf
Tam Pham <phamttam17@gmail.com>
2020-05-03 11:59:19 +0800

    add autocor

--- neurokit2/stats/__init__.py
+++ neurokit2/stats/__init__.py
@@ -4,6 +4,7 @@ from .standardize import standardize
 from .hdi import hdi
 from .mad import mad
 from .correlation import cor
+from .correlation import autocor
 from .density import density
 from .distance import distance
 from .rescale import rescale
--- neurokit2/stats/correlation.py
+++ neurokit2/stats/correlation.py
@@ -50,3 +50,7 @@ def _cor_methods(x, y, method="pearson"):
         raise ValueError("NeuroKit error: cor(): 'method' not recognized.")
 
     return r, p
+
+def autocor(x):
+    result = np.correlate(x, x, mode='full')
+    return result[result.size // 2:]
