460b61637c7db1417c760e55f14a9dfc6be7f1e1
DominiqueMakowski <dom.mak19@gmail.com>
2020-05-09 18:15:33 +0800

    improve dfa

--- neurokit2/complexity/fractal_dfa.py
+++ neurokit2/complexity/fractal_dfa.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 
 from ..misc import range_log
-from ..stats.correlation import _cor_plot
 
 
-def fractal_dfa(signal, windows="default", overlap=True, integrate=True, order=1, show=False):
+def fractal_dfa(signal, windows="default", overlap=True, integrate=True, order=1, show=False, **kwargs):
     """Detrended Fluctuation Analysis (DFA)
 
     Computes Detrended Fluctuation Analysis (DFA) on the time series data. Detrended fluctuation analysis, much like the Hurst exponent, is used to
@@ -39,8 +39,7 @@ def fractal_dfa(signal, windows="default", overlap=True, integrate=True, order=1
     >>> import neurokit2 as nk
     >>>
     >>> signal = nk.signal_simulate(duration=1)
-    >>> nk.fractal_dfa(signal)
-    2.0262713695244083
+    >>> nk.fractal_dfa(signal)[2], windows=[10, 20, 40, 70])
 
 
     References
@@ -60,22 +59,13 @@ def fractal_dfa(signal, windows="default", overlap=True, integrate=True, order=1
     # Divide profile
     fluctuations = np.zeros(len(windows))
     for i, window in enumerate(windows):
+
         # Get window
         segments = _fractal_dfa_getwindow(signal, n, window, overlap=overlap)
 
-        # Local trend
-        x = np.arange(window)
-        j_segments = np.arange(len(segments))
-
-        poly = np.array([np.polyfit(x, segments[j], order) for j in j_segments])
-        trend = np.array([np.polyval(poly[j], x) for j in j_segments])
-
-        # Calculate fluctuation around trend
-        fluctuation = np.sqrt(np.sum((segments - trend) ** 2, axis=1) / window)
-
         # Mean fluctuation
-        mean_fluctuation = np.sum(fluctuation) / len(fluctuation)
-        fluctuations[i] = mean_fluctuation
+        fluctuations[i] = _fractal_dfa_fluctuation(segments, window, order=order, **kwargs)
+
 
     # Filter zeros
     nonzero = np.nonzero(fluctuations)[0]
@@ -84,14 +74,14 @@ def fractal_dfa(signal, windows="default", overlap=True, integrate=True, order=1
 
     # Compute trend
     if len(fluctuations) == 0:
-        dfa = np.nan
+        return np.nan
     else:
-        dfa = np.polyfit(np.log(windows), np.log(fluctuations), order)[0]
+        dfa = np.polyfit(np.log(windows), np.log(fluctuations), order)
 
     if show is True:
-        _cor_plot(np.log(windows), np.log(fluctuations))
+        _fractal_dfa_plot(windows, fluctuations, dfa)
 
-    return dfa
+    return dfa[0]
 
 
 # =============================================================================
@@ -116,6 +106,7 @@ def _fractal_dfa_findwindows(signal, n, windows='default'):
     if windows is None or isinstance(windows, str):
         if n >= 80:
             windows = range_log(4, 0.1 * n, 1.2)  # Default window
+            # scale_lim=[5,9], scale_dens=0.25; scales = (2**np.arange(scale_lim[0], scale_lim[1], scale_dens)).astype(np.int)
         else:
             raise ValueError("NeuroKit error: fractal_dfa(): signal is too short to compute DFA.")
 
@@ -127,3 +118,36 @@ def _fractal_dfa_findwindows(signal, n, windows='default'):
     if np.max(windows) >= n:
         raise ValueError("NeuroKit error: fractal_dfa(): the window cannot contain more data points than the time series.")
     return windows
+
+
+
+def _fractal_dfa_plot(windows, fluctuations, dfa):
+    fluctfit = 2**np.polyval(dfa, np.log2(windows))
+    plt.loglog(windows, fluctuations, 'bo')
+    plt.loglog(windows, fluctfit, 'r', label=r'$\alpha$ = %0.2f' %dfa[0])
+    plt.title('DFA')
+    plt.xlabel(r'$\log_{10}$(time window)')
+    plt.ylabel(r'$\log_{10}$<F(t)>')
+    plt.legend()
+    plt.show()
+
+
+
+def _fractal_dfa_fluctuation(segments, window, order=1, rms=False):
+    # Local trend
+    x = np.arange(window)
+    j_segments = np.arange(len(segments))
+
+    poly = np.array([np.polyfit(x, segments[j], order) for j in j_segments])
+    trend = np.array([np.polyval(poly[j], x) for j in j_segments])
+
+    if rms is True:
+        # Method from https://github.com/dokato/dfa/blob/master/dfa.py byDominik Krzeminski
+        rms = np.sqrt(np.mean((segments - trend)**2))
+        fluctuation = np.sqrt(np.mean(rms**2))
+    else:
+        # Calculate fluctuation around trend
+        fluctuation = np.sqrt(np.sum((segments - trend) ** 2, axis=1) / window)
+        # Mean fluctuation
+        fluctuation = np.sum(fluctuation) / len(fluctuation)
+    return fluctuation
