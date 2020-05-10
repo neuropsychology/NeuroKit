4df0aacf2663671a3cd413da87c9c6761e120aaa
DominiqueMakowski <dom.mak19@gmail.com>
2020-05-10 15:43:18 +0800

    correlation dimension

--- neurokit2/complexity/fractal_correlation.py
+++ neurokit2/complexity/fractal_correlation.py
@@ -4,10 +4,12 @@ import sklearn.metrics.pairwise
 import matplotlib.pyplot as plt
 
 from .embedding import embedding
+from ..misc import expspace
 
-
-def fractal_correlation(signal, method="nolds", delay=1, dimension=2, show=False):
+def fractal_correlation(signal, delay=1, dimension=2, r=64, show=False):
     """Correlation Dimension
+
+    Python implementation of the Correlation Dimension D2 of a signal.
 
     Parameters
     ----------
@@ -17,11 +19,13 @@ def fractal_correlation(signal, method="nolds", delay=1, dimension=2, show=False
         Time delay (often denoted 'Tau', sometimes referred to as 'lag'). In practice, it is common to have a fixed time lag (corresponding for instance to the sampling rate; Gautama, 2003), or to find a suitable value using some algorithmic heuristics (see ``delay_optimal()``).
     dimension : int
         Embedding dimension (often denoted 'm' or 'd', sometimes referred to as 'order'). Typically 2 or 3. It corresponds to the number of compared runs of lagged data. If 2, the embedding returns an array with two columns corresponding to the original signal and its delayed (by Tau) version.
+    r : str, int or list
+        The sequence of radiuses to test. If an integer is passed, will get an exponential sequence ranging from 2.5% to 50% of the distance range. Methods implemented in other packages can be used via setting ``r='nolds'`` or ``r='Corr_Dim'``.
 
     Returns
     ----------
-    D : float
-        The correlation dimension.
+    D2 : float
+        The correlation dimension D2.
 
     Examples
     ----------
@@ -29,42 +33,48 @@ def fractal_correlation(signal, method="nolds", delay=1, dimension=2, show=False
     >>>
     >>> signal = nk.signal_simulate(duration=2, frequency=5)
     >>>
-    >>> nk.fractal_correlation(signal, method="nolds", show=True)
-    >>> nk.fractal_correlation(signal, method="Corr_Dim", show=True)
+    >>> nk.fractal_correlation(signal, r="nolds", show=True)
+    >>> nk.fractal_correlation(signal, r=32, show=True)
+    >>>
+    >>> signal = nk.rsp_simulate(duration=120, sampling_rate=50)
+    >>>
+    >>> nk.fractal_correlation(signal, r="nolds", show=True)
+    >>> nk.fractal_correlation(signal, r=32, show=True)
 
 
     References
     -----------
+    - Boon, M. Y., Henry, B. I., Suttle, C. M., & Dain, S. J. (2008). The correlation dimension: A useful objective measure of the transient visual evoked potential?. Journal of vision, 8(1), 6-6.
     - `nolds <https://github.com/CSchoel/nolds/blob/master/nolds/measures.py>`_
+    - `Corr_Dim <https://github.com/jcvasquezc/Corr_Dim>`_
     """
     embedded = embedding(signal, delay=delay, dimension=dimension)
     dist = sklearn.metrics.pairwise.euclidean_distances(embedded)
 
-    if method == "nolds":
-        r_vals = _fractal_correlation_get_r(signal, method=1)
-        r_vals, corr = _fractal_correlation1(signal, r_vals, dist)
-    else:
-        r_vals = _fractal_correlation_get_r(dist, method=2)
-        r_vals, corr = _fractal_correlation2(embedded, r_vals, dist)
+    r_vals = _fractal_correlation_get_r(r, signal, dist)
 
+    r_vals, corr = _fractal_correlation(signal, r_vals, dist)
+
+    # Corr_Dim method: https://github.com/jcvasquezc/Corr_Dim
+#    r_vals, corr = _fractal_correlation_Corr_Dim(embedded, r_vals, dist)
 
     # Compute trend
     if len(corr) == 0:
         return np.nan
     else:
-        d = np.polyfit(np.log2(r_vals), np.log2(corr), 1)
+        d2 = np.polyfit(np.log2(r_vals), np.log2(corr), 1)
 
 
     if show is True:
-        _fractal_correlation_plot(r_vals, corr, d)
+        _fractal_correlation_plot(r_vals, corr, d2)
 
-    return d[0]
+    return d2[0]
 
 
 # =============================================================================
 # Methods
 # =============================================================================
-def _fractal_correlation1(signal, r_vals, dist):
+def _fractal_correlation(signal, r_vals, dist):
     """
     References
     -----------
@@ -72,11 +82,9 @@ def _fractal_correlation1(signal, r_vals, dist):
     """
     n = len(signal)
 
-
     corr = np.zeros(len(r_vals))
     for i, r in enumerate(r_vals):
         corr[i] = 1 / (n * (n - 1)) * np.sum(dist < r)
-
 
     # filter zeros from csums
     nonzero = np.nonzero(corr)[0]
@@ -87,7 +95,7 @@ def _fractal_correlation1(signal, r_vals, dist):
 
 
 
-def _fractal_correlation2(embedded, r_vals, dist):
+def _fractal_correlation_Corr_Dim(embedded, r_vals, dist):
     """
     References
     -----------
@@ -114,18 +122,31 @@ def _fractal_correlation2(embedded, r_vals, dist):
 # =============================================================================
 # Utilities
 # =============================================================================
-def _fractal_correlation_get_r(signal, method=1):
-    if method == 1:
-        sd = np.std(signal, ddof=1)
-        r_vals = _fractal_correlation_range_r(0.1 * sd, 0.5 * sd, 1.03)
-    else:
-        max_r = np.max(signal)
-        min_r = np.min(signal[np.where(signal > 0)])
-        max_r = np.exp(np.floor(np.log(max_r)))
-        n_r = np.int(np.floor(np.log(max_r / min_r))) + 1
+def _fractal_correlation_get_r(r, signal, dist, n=None):
+    if isinstance(r, str):
+        if r == "nolds":
+            sd = np.std(signal, ddof=1)
+            min_r, max_r, factor = 0.1 * sd, 0.5 * sd, 1.03
 
-        ones = -1 * np.ones([n_r])
-        r_vals = max_r * np.exp(ones * np.arange(n_r) - ones)
+            r_n = int(np.floor(np.log(1.0 * max_r / min_r) / np.log(factor)))
+            r_vals = np.array([min_r * (factor ** i) for i in range(r_n + 1)])
+
+        elif r == "Corr_Dim":
+            r_min, r_max = np.min(dist[np.where(dist > 0)]), np.exp(np.floor(np.log(np.max(dist))))
+
+            n_r = np.int(np.floor(np.log(r_max / r_min))) + 1
+
+            ones = -1 * np.ones([n_r])
+            r_vals = r_max * np.exp(ones * np.arange(n_r) - ones)
+
+        elif r == "boon2008":
+            r_min, r_max = np.min(dist[np.where(dist > 0)]), np.max(dist)
+            r_vals = r_min + np.arange(1, 65) * ((r_max - r_min) / 64)
+
+    if isinstance(r, int):
+        dist_range = np.max(dist) - np.min(dist)
+        r_min, r_max =  (np.min(dist) + 0.025 * dist_range), (np.min(dist) + 0.5 * dist_range)
+        r_vals = np.exp2(np.linspace(np.log2(r_min), np.log2(r_max), r, endpoint=True))
 
     return r_vals
 
@@ -134,36 +155,11 @@ def _fractal_correlation_get_r(signal, method=1):
 
 
 
-def _fractal_correlation_range_r(min_n, max_n, factor):
-  """
-  Creates a list of values by successively multiplying a minimum value min_n by
-  a factor > 1 until a maximum value max_n is reached.
-  Args:
-    min_n (float):
-      minimum value (must be < max_n)
-    max_n (float):
-      maximum value (must be > min_n)
-    factor (float):
-      factor used to increase min_n (must be > 1)
-  Returns:
-    list of floats:
-      min_n, min_n * factor, min_n * factor^2, ... min_n * factor^i < max_n
-  """
-  assert max_n > min_n
-  assert factor > 1
 
-  max_i = int(np.floor(np.log(1.0 * max_n / min_n) / np.log(factor)))
-
-  return np.array([min_n * (factor ** i) for i in range(max_i + 1)])
-
-
-
-
-
-def _fractal_correlation_plot(r_vals, corr, d):
-    fit = 2**np.polyval(d, np.log2(r_vals))
+def _fractal_correlation_plot(r_vals, corr, d2):
+    fit = 2**np.polyval(d2, np.log2(r_vals))
     plt.loglog(r_vals, corr, 'bo')
-    plt.loglog(r_vals, fit, 'r', label=r'$D$ = %0.3f' % d[0])
+    plt.loglog(r_vals, fit, 'r', label=r'$D2$ = %0.3f' % d2[0])
     plt.title('Correlation Dimension')
     plt.xlabel(r'$\log_{10}$(r)')
     plt.ylabel(r'$\log_{10}$(c)')
