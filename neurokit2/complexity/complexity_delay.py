c7325c45edd9aecb33d9d34765503e0321675b03
Zen <zen-juen@users.noreply.github.com>
2020-05-20 14:34:52 +0800

    include 3D plot option in _embedding_delay_plot

--- neurokit2/complexity/complexity_delay.py
+++ neurokit2/complexity/complexity_delay.py
@@ -163,45 +163,59 @@ def _embedding_delay_metric(signal, tau_sequence, metric="Mutual Information"):
 # =============================================================================
 # Internals
 # =============================================================================
-def _embedding_delay_plot(signal, metric_values, tau_sequence, tau=1, metric="Mutual Information", ax0=None, ax1=None):
+def _embedding_delay_plot(signal, metric_values, tau_sequence, tau=1, metric="Mutual Information", ax0=None, ax1=None, plot='2D'):
     """
     """
+    # Prepare figure
     if ax0 is None and ax1 is None:
         fig = plt.figure(constrained_layout=False)
         spec = matplotlib.gridspec.GridSpec(ncols=1, nrows=2, height_ratios=[1, 3], width_ratios=[2])
-
         ax0 = fig.add_subplot(spec[0])
-        ax1 = fig.add_subplot(spec[1])
+        if plot == '2D':
+            ax1 = fig.add_subplot(spec[1])
+        elif plot == '3D':
+            ax1 = fig.add_subplot(spec[1], projection='3d')
     else:
         fig = None
 
-    # Upper plot (metric evolution)
     ax0.set_title("Optimization of Delay (tau)")
     ax0.set_xlabel("Time Delay (tau)")
     ax0.set_ylabel(metric)
     ax0.plot(tau_sequence, metric_values, color='#2196F3')
     ax0.axvline(x=tau, color='#E91E63', label='Optimal delay: ' + str(tau))
     ax0.legend(loc='upper right')
+    ax1.set_title("Attractor")
+    ax1.set_xlabel("Signal [i]")
+    ax1.set_ylabel("Signal [i-" + str(tau) + "]")
 
-    # Attractor
+    # Get data points, set axis limits
     embedded = complexity_embedding(signal, delay=tau, dimension=3)
     x = embedded[:, 0]
     y = embedded[:, 1]
     z = embedded[:, 2]
-
-    #   Chunk the data into colorbars
-    points = np.array([x, y]).T.reshape(-1, 1, 2)
-    segments = np.concatenate([points[:-1], points[1:]], axis=1)
-    norm = plt.Normalize(z.min(), z.max())
-    lc = matplotlib.collections.LineCollection(segments, cmap='plasma', norm=norm)
-    lc.set_array(z)
-    line = ax1.add_collection(lc)
-
-    #   Customize
-    ax1.set_title("Attractor")
     ax1.set_xlim(x.min(), x.max())
     ax1.set_ylim(x.min(), x.max())
-    ax1.set_xlabel("Signal [i]")
-    ax1.set_ylabel("Signal [i-" + str(tau) + "]")
+
+    # Colors
+    norm = plt.Normalize(z.min(), z.max())
+    cmap = plt.get_cmap('plasma')
+    colors = cmap(norm(x))
+
+    # Attractor for 2D vs 3D
+    if plot == '2D':
+        points = np.array([x, y]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        lc = matplotlib.collections.LineCollection(segments, cmap='plasma', norm=norm)
+        lc.set_array(z)
+        line = ax1.add_collection(lc)
+
+    elif plot == '3D':
+        points = np.array([x, y, z]).T.reshape(-1, 1, 3)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        for i in range(len(x) - 1):
+            seg = segments[i]
+            l, = ax1.plot(seg[:, 0], seg[:, 1], seg[:, 2], color=colors[i])
+            l.set_solid_capstyle('round')
+        ax1.set_zlabel("Signal [i-" + str(2*tau) + "]")
 
     return fig
