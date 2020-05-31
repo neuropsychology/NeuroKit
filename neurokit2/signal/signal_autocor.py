047e54596b47c2f8e28e05f37e0c696a74e75472
Tam Pham <phamttam17@gmail.com>
2020-05-31 12:08:53 +0800

    signal_autocor to return 1 corr value if lag is specified

--- neurokit2/signal/signal_autocor.py
+++ neurokit2/signal/signal_autocor.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def signal_autocor(signal, normalize=True):
+def signal_autocor(signal, lag=None, normalize=True):
     """
     Auto-correlation of a 1-dimensional sequences.
 
@@ -11,12 +11,17 @@ def signal_autocor(signal, normalize=True):
         Vector of values.
     normalize : bool
         Normalize the autocorrelation output.
+    lag : int
+        Time lag.
+        If specified, one value of autocorrelation between signal with its lag self
+        will be returned.
 
     Returns
     -------
     r
         The cross-correlation of the signal with itself at different time lags.
         Minimum time lag is 0, maximum time lag is the length of the signal.
+        Or a correlation value at a specific lag if lag is not None.
 
     Examples
     --------
@@ -32,5 +37,8 @@ def signal_autocor(signal, normalize=True):
 
     if normalize is True:
         r = r / r[0]
+
+    if lag is not None:
+        r = r[lag]
 
     return r
