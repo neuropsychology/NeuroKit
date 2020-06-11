979eaa5a1df6ca70a2b45b924368aae13cb9f2df
Zen <zen-juen@users.noreply.github.com>
2020-06-11 11:47:11 +0800

    add eog_clean

--- neurokit2/eog/__init__.py
+++ neurokit2/eog/__init__.py
@@ -1,7 +1,6 @@
 """Submodule for NeuroKit."""
 
 from .eog_extract import eog_extract
+from .eog_clean import eog_clean
 
-__all__ = [
-    "eog_extract",
-]
+__all__ = ["eog_extract", "eog_clean"]
---
+++ neurokit2/eog/eog_clean.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from ..misc import as_vector
+from ..signal import signal_filter
+
+
+def eog_clean(eog_signal, sampling_rate=1000):
+    """Clean an EOG signal. Only Agarwal & Sivakumar (2019)'s method is implemented for now.
+
+    Prepare a raw EOG signal for eye blinks detection.
+
+    Parameters
+    ----------
+    eog_signal : list, array or Series
+        The raw EOG channel.
+    sampling_rate : int
+        The sampling frequency of `eog_signal` (in Hz, i.e., samples/second).
+        Defaults to 1000.
+
+    Returns
+    -------
+    array
+        Vector containing the cleaned EOG signal.
+
+    See Also
+    --------
+    eog_extract, signal_filter
+
+    Examples
+    --------
+    Examples
+    --------
+    >>> import neurokit2 as nk
+    >>>
+    >>> eog = nk.eog_extract(raw, channels=["124", "125"], resampling_rate=None, raw_return=True)
+    >>> sampling_rate = raw.info['sfreq']
+    >>> eog_cleaned = nk.eog_clean(eog, sampling_rate=sampling_rate)
+
+    References
+    ----------
+    - Agarwal, M., & Sivakumar, R. (2019, September). Blink: A Fully Automated Unsupervised Algorithm for
+    Eye-Blink Detection in EEG Signals. In 2019 57th Annual Allerton Conference on Communication, Control,
+    and Computing (Allerton) (pp. 1113-1121). IEEE.
+
+    """
+    eog_signal = as_vector(eog_signal)
+
+    # Filter
+    clean = signal_filter(eog_signal, sampling_rate=sampling_rate, method="butterworth", order=4,
+                          lowcut=None, highcut=10)
+
+    return clean
