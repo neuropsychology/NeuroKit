38300f6f075ed7b26c491ef98b5d3e92e1c8c979
Zen <zen-juen@users.noreply.github.com>
2020-06-14 16:46:10 +0800

    fix mne_channel_extract test

--- tests/tests_eeg.py
+++ tests/tests_eeg.py
@@ -10,7 +10,6 @@ import neurokit2 as nk
 
 def test_eeg_add_channel():
 
-
     raw = mne.io.read_raw_fif(mne.datasets.sample.data_path() + '/MEG/sample/sample_audvis_raw.fif', preload=True)
 
     # len(channel) > len(raw)
@@ -21,15 +20,14 @@ def test_eeg_add_channel():
     df1 = raw1.to_data_frame()
 
     # test if the column of channel is added
-    assert len(df1.columns) == 378
+    assert len(df1.columns) == 377
 
     # test if the NaN is appended properly to the added channel to account for difference in distance between two signals
-    sync_index_raw=100
-    sync_index_channel=0
+    sync_index_raw = 100
+    sync_index_channel = 0
     for i in df1["Added_Channel"].head(abs(sync_index_channel - sync_index_raw)):
         assert np.isnan(i)
     assert np.isfinite(df1["Added_Channel"].iloc[abs(sync_index_channel - sync_index_raw)])
-
 
     # len(channel) < len(raw)
     ecg2 = nk.ecg_simulate(length=166790)
@@ -39,11 +37,11 @@ def test_eeg_add_channel():
     df2 = raw2.to_data_frame()
 
     # test if the column of channel is added
-    assert len(df2.columns) == 378
+    assert len(df2.columns) == 377
 
     # test if the NaN is appended properly to the added channel to account for difference in distance between two signals + difference in length
-    sync_index_raw=0
-    sync_index_channel=100
+    sync_index_raw = 0
+    sync_index_channel = 100
     for i in df2["Added_Channel"].tail(abs(sync_index_channel - sync_index_raw) + (len(raw) - len(ecg2))):
         assert np.isnan(i)
     assert np.isfinite(df2["Added_Channel"].iloc[-abs(sync_index_channel - sync_index_raw) - (len(raw) - len(ecg2)) - 1])
@@ -54,15 +52,24 @@ def test_mne_channel_extract():
     raw = mne.io.read_raw_fif(mne.datasets.sample.data_path() + '/MEG/sample/sample_audvis_raw.fif', preload=True)
 
     # Extract 1 channel
-    name = 'EEG 053'
+    what = 'EEG 053'
 
-    raw_channel = nk.mne_channel_extract(raw, name)
-    assert raw_channel.name == name
+    raw_channel = nk.mne_channel_extract(raw, what)
+    assert raw_channel.what == what
 
     # Extract more than 1 channel
-    name2 = ['EEG 053', 'EEG 054', 'EEG 055']
+    what2 = ['EEG 053', 'EEG 054', 'EEG 055']
 
-    raw_channel2 = nk.mne_channel_extract(raw, name2)
+    raw_channel2 = nk.mne_channel_extract(raw, what2)
     assert len(raw_channel2.columns) == 3
-    assert all(elem in name2
+    assert all(elem in what2
                for elem in np.array(raw_channel2.columns.values, dtype=str))
+
+    # Extract a category of channels
+    what3 = 'EEG'
+
+    raw_channels = nk.mne_channel_extract(raw, what3)
+    assert len(raw_channels.columns) == 60
+
+    raw_eeg_names = [x for x in raw.info['ch_names'] if what3 in x]
+    assert raw_eeg_names == list(raw_channels.columns.values)
