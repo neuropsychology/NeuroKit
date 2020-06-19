33c4223baf6836e083dc83f2c077e96d3103dd37
Tam Pham <phamttam17@gmail.com>
2020-06-19 14:33:01 +0800

    WIP add burg method
    #267

--- neurokit2/signal/signal_psd.py
+++ neurokit2/signal/signal_psd.py
@@ -177,3 +177,123 @@ def _signal_psd_lomb(
     power = np.asarray(scipy.signal.lombscargle(t, signal, angular_freqs, normalize=True))
 
     return frequency, power
+
+# =============================================================================
+# Burg method
+# =============================================================================
+def _signal_psd_burg(signal, order=15, criteria=None, corrected=True):
+
+    # Sanitize order and signal
+    if order <= 0.:
+        raise ValueError("Order must be > 0")
+    if order > len(signal):
+        raise ValueError("Order must be less than length signal minus 2")
+    if not isinstance(signal, np.ndarray):
+        signal = np.array(signal)
+
+    N = len(signal)
+
+    # Initialisation
+    # rho is variance of driving white noise process
+    rho = sum(abs(signal)**2.) / float(N)
+    den = rho * 2. * N
+
+    a = np.zeros(0, dtype=complex)
+    ref = np.zeros(0, dtype=complex)
+    ef = signal.astype(complex)
+    eb = signal.astype(complex)
+    temp = 1.
+
+    # Main recursion
+    residue_list = np.zeros(order + 1)
+
+    for k in range(0, order):
+
+        # calculate the next order reflection coefficient
+        num = sum([ef[j]*eb[j - 1].conjugate() for j in range(k + 1, N)])
+        den = temp * den - abs(ef[k])**2 - abs(eb[N - 1])**2
+        kp = -2. * num / den
+
+        temp = 1. - abs(kp)**2.
+        new_rho = temp * rho
+
+        if criteria is not None:
+            # k=k+1 because order goes from 1 to P whereas k starts at 0.
+            residue_new = _criteria(criteria=criteria, N=N, k=k+1, rho=new_rho, corrected=corrected)
+            if k == 0:
+                residue_old = 2. * residue_new
+
+            # Stop as criteria has reached
+            if residue_new > residue_old:
+                break
+
+        # This should be after the criteria
+        residue_old = residue_new
+        rho = new_rho
+        if rho <= 0:
+            raise ValueError("Found a negative value (expected positive strictly) %s. Decrease the order" % rho)
+
+        a.resize(a.size + 1)
+        a[k] = kp
+        if k == 0:
+            for j in range(N-1, k, -1):
+                save2 = ef[j]
+                ef[j] = save2 + kp * eb[j-1]  # Eq. (8.7)
+                eb[j] = eb[j-1] + kp.conjugate() * save2
+
+        else:
+            # update the AR coeff
+            khalf = (k + 1) // 2  # khalf must be an integer
+            for j in range(0, khalf):
+                ap = a[j] # previous value
+                a[j] = ap + kp * a[k-j-1].conjugate()  # Eq. (8.2)
+                if j != k-j-1:
+                    a[k-j-1] = a[k-j-1] + kp * ap.conjugate()  # Eq. (8.2)
+
+            # update the prediction error
+            for j in range(N-1, k, -1):
+                save2 = ef[j]
+                ef[j] = save2 + kp * eb[j-1]   # Eq. (8.7)
+                eb[j] = eb[j-1] + kp.conjugate() * save2
+
+        # save the reflection coefficient
+        ref.resize(ref.size+1)
+        ref[k] = kp
+
+    return a, rho, ref
+
+# =============================================================================
+# Utilities
+# =============================================================================
+def _criteria(criteria=None, N=None, k=None, rho=None, corrected=True):
+    """criteria to automatically select order in parametric PSD
+    AIC, AICc, KIC and AKICc are based on information theory. They attempt to balance the complexity
+    (or length) of the model against how well the model fits the data.
+    AIC and KIC are biased estimates of the asymmetric and the symmetric Kullback-Leibler divergence
+    respectively.
+    AICc and AKICc attempt to correct the bias.\
+
+    Parameters
+    ----------
+    criteria : str
+        The criteria to be used.
+    N : int
+        The sample size of the signal
+    k : list, array
+        The list of AR order.
+    rho : list, array
+        The list of rho at order k.
+    """
+    if criteria == "AIC":
+        if corrected is True:
+            residue = np.log(rho) + 2. * (k + 1) / (N - k - 2)
+        else:
+            residue = N * np.log(np.array(rho)) + 2.* (np.array(k) + 1)
+
+    elif criteria == "KIC":
+        if corrected is True:
+            residue = np.log(rho) + k/N/(N-k) + (3. - (k + 2.) / N) * (k + 1.) / (N - k - 2.)
+        else:
+            residue = np.log(rho) + 3. * (k + 1.) /float(N)
+
+    return residue
