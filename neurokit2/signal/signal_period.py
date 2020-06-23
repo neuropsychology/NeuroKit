11e862f769128e7c9b6b164f96cdece21b49971e
JohnDoenut <jan.c.brammer@gmail.com>
2020-06-23 15:12:32 +0200

    Edit docstring description of desired_length argument.

--- neurokit2/signal/signal_period.py
+++ neurokit2/signal/signal_period.py
@@ -19,12 +19,12 @@ def signal_period(peaks, sampling_rate=1000, desired_length=None, interpolation_
         The sampling frequency of the signal that contains peaks (in Hz, i.e., samples/second).
         Defaults to 1000.
     desired_length : int
-        If left at the default None, `desired_length` is set to the number of peaks. The returned
-        period will have the same number of elements as peaks. If set to a value larger than the
-        index of the last peaks, the returned period will be interpolated between peaks over
-        `desired_length` samples. To interpolate the period over the entire duration of the signal
-        that contains the peaks, set desired_length to the number of samples in the signal. Cannot be
-        smaller than or equal to the index of the last peak. Default to None.
+        If left at the default None, the returned period will have the same number of elements as peaks.
+        If set to a value larger than the sample at which the last peak occurs in the signal (i.e., peaks[-1]),
+        the returned period will be interpolated between peaks over `desired_length` samples. To interpolate
+        the period over the entire duration of the signal, set desired_length to the number of samples in the
+        signal. Cannot be smaller than or equal to the sample at which the last peak occurs in the signal.
+        Defaults to None.
     interpolation_method : str
         Method used to interpolate the rate between peaks. See `signal_interpolate()`. 'monotone_cubic' is chosen
         as the default interpolation method since it ensures monotone interpolation between data points
