eae53f6887c33f41e970046eb0e10e07e22b6bbc
DominiqueMakowski <dom.mak19@gmail.com>
2020-06-24 12:58:32 +0800

    add find_consecutive misc function

---
+++ neurokit2/misc/find_consecutive.py
@@ -0,0 +1,29 @@
+import numpy as np
+import itertools
+
+def find_consecutive(x):
+    """Find and group consecutive values in a list.
+
+    Creates a list of integer values of a given length from start to stop, spread by an exponential function.
+
+    Parameters
+    ----------
+    x : list
+        The list to look in.
+
+    Returns
+    -------
+    list
+        A list of tuples corresponding to groups containing all the consecutive numbers.
+
+    Examples
+    ---------
+    >>> import neurokit2 as nk
+    >>>
+    >>> x =[2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 20]
+    >>> nk.find_consecutive(x)
+    [(2, 3, 4, 5), (12, 13, 14, 15, 16, 17), (20,)]
+
+    """
+
+    return [tuple(g) for k, g in itertools.groupby(x, lambda n, c=itertools.count(): n-next(c))]
