15cfbfec11b19951c76d8b67084963b384186d2e
DominiqueMakowski <dom.mak19@gmail.com>
2020-06-25 08:54:40 +0800

    add ppg_process

--- neurokit2/ppg/__init__.py
+++ neurokit2/ppg/__init__.py
@@ -5,6 +5,7 @@ from ..signal import signal_rate as ppg_rate
 from .ppg_clean import ppg_clean
 from .ppg_findpeaks import ppg_findpeaks
 from .ppg_simulate import ppg_simulate
+from .ppg_process import ppg_process
 
 
-__all__ = ["ppg_simulate", "ppg_clean", "ppg_findpeaks", "ppg_rate"]
+__all__ = ["ppg_simulate", "ppg_clean", "ppg_findpeaks", "ppg_rate", "ppg_process"]
