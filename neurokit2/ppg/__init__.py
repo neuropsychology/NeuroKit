28c74831bb5776b5ec67da932fc4852b5964abc2
Zen <zen-juen@users.noreply.github.com>
2020-06-25 17:48:26 +0800

    add ppg_plot :smile:

--- neurokit2/ppg/__init__.py
+++ neurokit2/ppg/__init__.py
@@ -6,6 +6,7 @@ from .ppg_clean import ppg_clean
 from .ppg_findpeaks import ppg_findpeaks
 from .ppg_simulate import ppg_simulate
 from .ppg_process import ppg_process
+from .ppg_plot import ppg_plot
 
 
-__all__ = ["ppg_simulate", "ppg_clean", "ppg_findpeaks", "ppg_rate", "ppg_process"]
+__all__ = ["ppg_simulate", "ppg_clean", "ppg_findpeaks", "ppg_rate", "ppg_process", "ppg_plot"]
