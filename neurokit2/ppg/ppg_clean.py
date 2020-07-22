eaec6f01526807de074c7504f26ce3afa94a4e1a
Zen <zen-juen@users.noreply.github.com>
2020-07-22 23:15:50 +0800

    formatting ppg_clean

--- neurokit2/ppg/ppg_clean.py
+++ neurokit2/ppg/ppg_clean.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-
 from ..misc import as_vector
 from ..signal import signal_filter
 
 
-def ppg_clean(ppg_signal, sampling_rate=1000, method="elgendi"):
+def ppg_clean(ppg_signal, sampling_rate=1000, heart_rate=None, method="elgendi"):
     """Clean a photoplethysmogram (PPG) signal.
 
     Prepare a raw PPG signal for systolic peak detection.
@@ -13,12 +12,13 @@ def ppg_clean(ppg_signal, sampling_rate=1000, method="elgendi"):
     ----------
     ppg_signal : Union[list, np.array, pd.Series]
         The raw PPG channel.
+    heart_rate : Union[int, float]
+        The heart rate of the PPG signal. Applicable only if method is "nabian2018" to check
+        that filter frequency is appropriate.
     sampling_rate : int
         The sampling frequency of the PPG (in Hz, i.e., samples/second). The default is 1000.
     method : str
         The processing pipeline to apply. Can be one of "elgendi" or "nabian2018". The default is "elgendi".
-    highcut : int
-        The cut-off frequency for removal of high frequency artifacts in BP signals (in Hz, i.e., samples/second). The default is 40.
 
     Returns
     -------
@@ -35,17 +35,22 @@ def ppg_clean(ppg_signal, sampling_rate=1000, method="elgendi"):
     >>> import matplotlib.pyplot as plt
     >>>
     >>> ppg = nk.ppg_simulate(heart_rate=75, duration=30)
-    >>> ppg_clean = nk.ppg_clean(ppg)
+    >>> ppg_elgendi = nk.ppg_clean(ppg, method='elgendi')
+    >>> ppg_nabian = nk.ppg_clean(ppg, method='nabian2018', heart_rate=75)
     >>>
-    >>> plt.plot(ppg, label="raw PPG") #doctest: +SKIP
-    >>> plt.plot(ppg_clean, label="clean PPG") #doctest: +SKIP
-    >>> plt.legend() #doctest: +SKIP
+    >>> # Plot to compare methods
+    >>> signals = pd.DataFrame({"PPG_Raw" : ppg,
+    ...                         "PPG_Elgendi" : ppg_elgendi,
+    ...                         "PPG_Nabian" : ppg_nabian})
+     >>> signals.plot() #doctest: +ELLIPSIS
+     <matplotlib.axes._subplots.AxesSubplot object at ...>
 
     References
     ----------
-    - Nabian, M., Yin, Y., Wormwood, J., Quigley, K. S., Barrett, L. F., &amp; Ostadabbas, S. (2018). An Open-Source 
-    Feature Extraction Tool for the Analysis of Peripheral Physiological Data. IEEE Journal of Translational Engineering in 
-    Health and Medicine, 6, 1-11. doi:10.1109/jtehm.2018.2878000
+    - Nabian, M., Yin, Y., Wormwood, J., Quigley, K. S., Barrett, L. F., &amp; Ostadabbas, S. (2018).
+    An Open-Source Feature Extraction Tool for the Analysis of Peripheral Physiological Data. IEEE Journal of
+    Translational Engineering in Health and Medicine, 6, 1-11. doi:10.1109/jtehm.2018.2878000
+
     """
     ppg_signal = as_vector(ppg_signal)
 
@@ -53,12 +58,15 @@ def ppg_clean(ppg_signal, sampling_rate=1000, method="elgendi"):
     if method in ["elgendi"]:
         clean = _ppg_clean_elgendi(ppg_signal, sampling_rate)
     elif method in ["nabian2018"]:
-        clean = _ppg_clean_nabian2018(ppg_signal, sampling_rate, highcut=highcut)
+        clean = _ppg_clean_nabian2018(ppg_signal, sampling_rate, heart_rate=heart_rate)
     else:
         raise ValueError("Neurokit error: Please use one of the following methods: 'elgendi' or 'nabian2018'.")
 
     return clean
 
+# =============================================================================
+# Methods
+# =============================================================================
 
 def _ppg_clean_elgendi(ppg_signal, sampling_rate):
 
@@ -67,18 +75,22 @@ def _ppg_clean_elgendi(ppg_signal, sampling_rate):
     )
     return filtered
 
-def _ppg_clean_nabian2018 (ppg_signal, sampling_rate, highcut="default"):
-    
-    """Low-pass filter for continuous BP signal preprocessing"""
 
-    filtered = signal_filter(ppg_signal, sampling_rate=sampling_rate, lowcut=None, highcut="default", order=2, method="butterworth")
-    
-    if highcut == "default":
-        highcut == 40
-        
-    if highcut is not "default":
-        highcut>=(10*heart_rate) and highcut<(0.5*sampling_rate)
-    else:
-        raise ValueError("Neurokit error: Highcut value should be at least 10 times heart rate and less than 0.5 times sampling rate.")
-    
+def _ppg_clean_nabian2018(ppg_signal, sampling_rate, heart_rate=None):
+    """Low-pass filter for continuous BP signal preprocessing, adaopted from Nabian et al. (2018).
+    """
+
+    # Determine low-pass filter value
+    highcut = 40
+
+    # Convert heart rate to seconds, check if low-pass filter within appropriate range
+    if heart_rate is not None:
+        heart_rate = heart_rate / 60
+
+        if not highcut >= 10*heart_rate and not highcut < 0.5*sampling_rate:
+            raise ValueError("Neurokit error: Highcut value should be at least 10 times heart rate and"
+                             " less than 0.5 times sampling rate.")
+
+    filtered = signal_filter(ppg_signal, sampling_rate=sampling_rate, lowcut=None, highcut=highcut, order=2, method="butterworth")
+
     return filtered
