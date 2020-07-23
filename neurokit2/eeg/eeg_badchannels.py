9e0c7ffcd8d0c03f78c3b80ad692370077775f79
DominiqueMakowski <dom.mak19@gmail.com>
2020-07-23 23:48:08 +0800

    minor

--- neurokit2/eeg/eeg_badchannels.py
+++ neurokit2/eeg/eeg_badchannels.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 
-from ..stats import standardize, mad
+from ..stats import standardize, mad, hdi
 from ..signal import signal_zerocrossings
 
 
 
 
 def eeg_badchannels(eeg, bad_threshold=0.5, distance_threshold=0.99):
-    """Find bad channels
+    """Find bad channels.
 
     Parameters
     ----------
@@ -43,11 +43,17 @@ def eeg_badchannels(eeg, bad_threshold=0.5, distance_threshold=0.99):
                 "NeuroKit error: eeg_badchannels(): the 'mne' module is required for this function"
                 " to run. Please install it first (`pip install mne`).",
             )
-        eeg, time = eeg[mne.pick_types(eeg.info, eeg=True)]
+        selection = mne.pick_types(eeg.info, eeg=True)
+        ch_names = np.array(eeg.ch_names)[selection]
+        eeg, time = eeg[selection]
+    else:
+        ch_names = np.arange(len(eeg))
 
     results = []
     for i in range(len(eeg)):
         channel = eeg[i, :]
+
+        hdi_values = hdi(channel, ci=0.90)
         info = {"Channel": [i],
                 "SD": [np.nanstd(channel, ddof=1)],
                 "Mean": [np.nanmean(channel)],
@@ -56,6 +62,8 @@ def eeg_badchannels(eeg, bad_threshold=0.5, distance_threshold=0.99):
                 "Skewness": [scipy.stats.skew(channel)],
                 "Kurtosis": [scipy.stats.kurtosis(channel)],
                 "Amplitude": [np.max(channel) - np.min(channel)],
+                "CI_low": [hdi_values[0]],
+                "CI_high": [hdi_values[1]],
                 "n_ZeroCrossings": [len(signal_zerocrossings(channel - np.nanmean(channel)))]}
         results.append(pd.DataFrame(info))
     results = pd.concat(results, axis=0)
@@ -63,6 +71,6 @@ def eeg_badchannels(eeg, bad_threshold=0.5, distance_threshold=0.99):
 
     z = standardize(results)
     results["Bad"] = (z.abs() > scipy.stats.norm.ppf(distance_threshold)).sum(axis=1) / len(results.columns)
-    bads = np.where(results["Bad"] >= bad_threshold)[0]
+    bads = ch_names[np.where(results["Bad"] >= bad_threshold)[0]]
 
-    return bads, results
+    return list(bads), results
--- studies/microstates_howmany/script.py
+++ studies/microstates_howmany/script.py
@@ -6,7 +6,7 @@ import pandas as pd
 import neurokit2 as nk
 import matplotlib.pyplot as plt
 import autoreject
-from autoreject.utils import interpolate_bads  # noqa
+from autoreject.utils import interpolate_bads
 import scipy.stats
 
 
@@ -29,26 +29,39 @@ for i, file in enumerate(files):
                             event_conditions=["EyesClosed", "EyesOpen"] * 4)
 
     # Rereference
-    raw = nk.eeg_rereference(raw, "average")
+    raw = nk.eeg_rereference(raw, ["M1", "M2"])
 
     # Filter
     raw = raw.filter(1, 35)
 
+    # Bad epochs
+    bads, info = nk.eeg_badchannels(raw)
+    raw.info['bads'] += bads
+#    raw.plot()
+    raw = raw.interpolate_bads()
+
     # ICA
     ica = mne.preprocessing.ICA(n_components=15, random_state=97).fit(raw)
-    ica.plot_properties(raw, picks=[0, 1])
-
     ica = ica.detect_artifacts(raw, eog_ch=['LVEOG', 'RVEOG'])
+#    ica.plot_properties(raw, picks=ica.exclude)
     raw = ica.apply(raw)
 
+    # Rereference
+    raw = nk.eeg_rereference(raw, "average")
 
-    ransac = autoreject.Ransac(verbose='progressbar', picks="eeg", n_jobs=1)
-    raw = autoreject.get_rejection_threshold(raw, picks="eeg")
 
-
-raw.info["ch_names"]
-nk.signal_plot(event)
-mne.viz.plot_raw(raw)
+#
+#
+#
+#
+#
+#    ransac = autoreject.Ransac(verbose='progressbar', picks="eeg", n_jobs=1)
+#    raw = autoreject.get_rejection_threshold(raw, picks="eeg")
+#
+#
+#raw.info["ch_names"]
+#nk.signal_plot(event)
+#mne.viz.plot_raw(raw)
 
 
 
