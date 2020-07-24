dc4ce294aa40c0a2495d938633e89383939458dc
Elaine Teo <66601704+elaineteo2000@users.noreply.github.com>
2020-07-24 22:17:19 +0800

    minor edit

--- neurokit2/ppg/ppg_clean.py
+++ neurokit2/ppg/ppg_clean.py
@@ -13,7 +13,7 @@ def ppg_clean(ppg_signal, sampling_rate=1000, heart_rate=None, method="elgendi")
     ppg_signal : Union[list, np.array, pd.Series]
         The raw PPG channel.
     heart_rate : Union[int, float]
-        The heart rate of the PPG signal. Applicable only if method is "nabian2018" to check
+        The heart rate of the PPG signal. Applicable only if method is "nabian2018" to check that
         filter frequency is appropriate.
     sampling_rate : int
         The sampling frequency of the PPG (in Hz, i.e., samples/second). The default is 1000.
