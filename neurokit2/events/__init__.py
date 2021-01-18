0ca063ba709ccbdb70e215755240157feeaef8d3
Dominique Makowski <dom.mak19@gmail.com>
2021-01-18 17:40:54 +0800

    add `events_create()` function to facilitate events dict creation #405

--- neurokit2/events/__init__.py
+++ neurokit2/events/__init__.py
@@ -1,8 +1,8 @@
 """Submodule for NeuroKit."""
 
 from .events_find import events_find
+from .events_create import events_create
 from .events_plot import events_plot
 from .events_to_mne import events_to_mne
 
-
-__all__ = ["events_find", "events_plot", "events_to_mne"]
+__all__ = ["events_find", "events_create", "events_plot", "events_to_mne"]
