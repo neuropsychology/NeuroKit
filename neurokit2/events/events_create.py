926c2d1aacdc063302d6e75f8367b5557d614363
DominiqueMakowski <dom.mak19@gmail.com>
2021-01-19 12:14:58 +0800

    Update events_create.py

--- neurokit2/events/events_create.py
+++ neurokit2/events/events_create.py
@@ -1,15 +1,17 @@
 import numpy as np
+
 from .events_find import _events_find_label
 
+
 def events_create(event_onsets, event_durations=None, event_labels=None, event_conditions=None):
-    """Find and select events in a continuous signal (e.g., from a photosensor).
+    """Create events dictionnary from list of onsets.
 
     Parameters
     ----------
     event_onsets : array or list
         A list of events onset.
     event_durations : array or list
-        A list of durations. If none is passed, will take the duration 
+        A list of durations. If none is passed, will take the duration
         between each onset (i.e., will assume that events are consecutive).
     event_labels : list
         A list containing unique event identifiers. If `None`, will use the event index number.
@@ -40,10 +42,10 @@ def events_create(event_onsets, event_durations=None, event_labels=None, event_c
     """
     if event_durations is None:
         event_durations = np.diff(np.concatenate(([0], event_onsets)))
-        
+
     events = {"onset": event_onsets,
               "duration": event_durations}
-        
+
     events = _events_find_label(events, event_labels=event_labels, event_conditions=event_conditions)
-    
+
     return events
