194abf0c9417033822bbb1fc608f4fc506c8a9fe
Zen <lauzenjuen@gmail.com>
2021-04-12 22:44:17 +0800

    add ppg_intervalrelated
    #438

--- neurokit2/ppg/__init__.py
+++ neurokit2/ppg/__init__.py
@@ -7,6 +7,8 @@ from .ppg_findpeaks import ppg_findpeaks
 from .ppg_plot import ppg_plot
 from .ppg_process import ppg_process
 from .ppg_simulate import ppg_simulate
+from .ppg_intervalrelated import ppg_intervalrelated
 
 
-__all__ = ["ppg_simulate", "ppg_clean", "ppg_findpeaks", "ppg_rate", "ppg_process", "ppg_plot"]
+__all__ = ["ppg_simulate", "ppg_clean", "ppg_findpeaks", "ppg_rate", "ppg_process", "ppg_plot",
+           "ppg_intervalrelated"]
