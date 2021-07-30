79ae8d2a7a3bf1a2406871e276c8e0239c7e40e4
Tam-Pham <phamttam17@gmail.com>
2021-07-30 14:44:33 +0800

    fix style

--- neurokit2/ecg/ecg_quality.py
+++ neurokit2/ecg/ecg_quality.py
@@ -19,7 +19,9 @@ def ecg_quality(ecg_cleaned, rpeaks=None, sampling_rate=1000, method="averageQRS
     The **"averageQRS"** method computes a continuous index of quality of the ECG signal, by interpolating the distance
     of each QRS segment from the average QRS segment present in the data. This index is
     therefore relative: 1 corresponds to heartbeats that are the closest to the average
-    sample and 0 corresponds to the most distant heartbeat from that average sample. Note that 1 does not necessarily means "good": if the majority of samples are bad, than being close to the average will likely mean bad as well. Use this index with care and plot it alongside your ECG signal to see if it makes sense.
+    sample and 0 corresponds to the most distant heartbeat from that average sample. Note that 1 does not necessarily means
+    "good": if the majority of samples are bad, than being close to the average will likely mean bad as well. Use this index
+    with care and plot it alongside your ECG signal to see if it makes sense.
 
     The **"zhao2018"** method (Zhao et la., 2018) extracts several signal quality indexes (SQIs):
     QRS wave power spectrum distribution pSQI, kurtosis kSQI, and baseline relative power basSQI.
