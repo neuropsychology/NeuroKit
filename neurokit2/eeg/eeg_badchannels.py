e668c9e7dd6a6d0d7bf51105105478f1957fcee8
Dominique Makowski <dom.mak19@gmail.com>
2021-09-19 17:56:20 +0800

    Update eeg_badchannels.py

--- neurokit2/eeg/eeg_badchannels.py
+++ neurokit2/eeg/eeg_badchannels.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 
-from ..stats import standardize, mad, hdi
 from ..signal import signal_zerocrossings
+from ..stats import hdi, mad, standardize
 
 
 def eeg_badchannels(eeg, bad_threshold=0.5, distance_threshold=0.99):
@@ -19,7 +19,7 @@ def eeg_badchannels(eeg, bad_threshold=0.5, distance_threshold=0.99):
         on which an observation is considered an outlier to be considered as bad. The default, 0.5,
         means that a channel must score as an outlier on half or more of the indices.
     distance_threshold : float
-        The quantile that desfines the absolute distance from the mean, i.e., the z-score for a
+        The quantile that defines the absolute distance from the mean, i.e., the z-score for a
         value of a variable to be considered an outlier. For instance, .975 becomes
         ``scipy.stats.norm.ppf(.975) ~= 1.96``. The default value (.99) means that all observations
         beyond 2.33 SD from the mean will be classified as outliers.
@@ -59,23 +59,27 @@ def eeg_badchannels(eeg, bad_threshold=0.5, distance_threshold=0.99):
         channel = eeg[i, :]
 
         hdi_values = hdi(channel, ci=0.90)
-        info = {"Channel": [i],
-                "SD": [np.nanstd(channel, ddof=1)],
-                "Mean": [np.nanmean(channel)],
-                "MAD": [mad(channel)],
-                "Median": [np.nanmedian(channel)],
-                "Skewness": [scipy.stats.skew(channel)],
-                "Kurtosis": [scipy.stats.kurtosis(channel)],
-                "Amplitude": [np.max(channel) - np.min(channel)],
-                "CI_low": [hdi_values[0]],
-                "CI_high": [hdi_values[1]],
-                "n_ZeroCrossings": [len(signal_zerocrossings(channel - np.nanmean(channel)))]}
+        info = {
+            "Channel": [i],
+            "SD": [np.nanstd(channel, ddof=1)],
+            "Mean": [np.nanmean(channel)],
+            "MAD": [mad(channel)],
+            "Median": [np.nanmedian(channel)],
+            "Skewness": [scipy.stats.skew(channel)],
+            "Kurtosis": [scipy.stats.kurtosis(channel)],
+            "Amplitude": [np.max(channel) - np.min(channel)],
+            "CI_low": [hdi_values[0]],
+            "CI_high": [hdi_values[1]],
+            "n_ZeroCrossings": [len(signal_zerocrossings(channel - np.nanmean(channel)))],
+        }
         results.append(pd.DataFrame(info))
     results = pd.concat(results, axis=0)
     results = results.set_index("Channel")
 
     z = standardize(results)
-    results["Bad"] = (z.abs() > scipy.stats.norm.ppf(distance_threshold)).sum(axis=1) / len(results.columns)
+    results["Bad"] = (z.abs() > scipy.stats.norm.ppf(distance_threshold)).sum(axis=1) / len(
+        results.columns
+    )
     bads = ch_names[np.where(results["Bad"] >= bad_threshold)[0]]
 
     return list(bads), results
