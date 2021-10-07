9633a91041b0dd2dca42ce7ca8d7b8523efa23cd
Dominique Makowski <dom.mak19@gmail.com>
2021-10-07 08:58:17 +0800

    Update complexity_k.py
    @Tam-Pham @zen-juen can you double check that it is korek

--- neurokit2/complexity/complexity_k.py
+++ neurokit2/complexity/complexity_k.py
@@ -8,16 +8,24 @@ from ..misc import NeuroKitWarning
 
 def complexity_k(signal, k_max="default", show=False):
     """
-    fractal_higuchi [summary]
+    The optimal kmax is computed based on the point at which HFD values plateau for a range of kmax values.
 
     Parameters
     ----------
-    signal : [type]
-        [description]
-    kmax : str, optional
-        [description], by default "default"
-    show : bool, optional
-        [description], by default False
+    signal : Union[list, np.array, pd.Series, np.ndarray, pd.DataFrame]
+        The signal (i.e., a time series) in the form of a vector of values or in
+        the form of an n-dimensional array (with a shape of len(channels) x len(samples))
+        or dataframe.
+    k_max : Union[int, str, list], optional
+        Maximum number of interval times (should be greater than or equal to 3) to be tested.
+    show : bool
+        Visualise the slope of the curve for the selected kmax value.
+
+    Examples
+    ----------
+    >>> import neurokit2 as nk
+    >>>
+    >>> signal = nk.signal_simulate(duration=1, frequency=[3, 6], noise = 0.2)
 
     Reference
     ----------
@@ -42,3 +50,31 @@ def complexity_k(signal, k_max="default", show=False):
             "k_max should be an int or a list of values of kmax to be tested.",
             category=NeuroKitWarning,
         )
+
+    # Get the slope
+    # -------------
+    slope_values = np.zeros(len(k_range))
+
+    average_values = _complexity_k_average_values(signal, k_range)
+
+
+# =============================================================================
+# Utilities
+# =============================================================================
+
+
+def _complexity_k_average_values(signal, k_range):
+    average_values = np.zeros(len(k_range))
+    # Compute length of the curve, Lm(k)
+    for i, k in enumerate(range(1, len(k_range) + 1)):
+        sets = []
+        for m in range(1, k + 1):
+            n_max = int(np.floor((len(signal) - m) / k))
+            normalization = (len(signal) - 1) / (n_max * k)
+            Lm_k = np.sum(np.abs(np.diff(signal[m - 1 :: k], n=1))) * normalization
+            Lm_k = Lm_k / k
+            sets.append(Lm_k)
+        # Compute average value over k sets of Lm(k)
+        L_k = np.sum(sets) / k
+        average_values[i] = L_k
+    return average_values
