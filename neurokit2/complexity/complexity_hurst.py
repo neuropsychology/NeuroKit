3dedafc0b90cc3ddc758e5668418cb1a2e1ec1af
Dominique Makowski <dom.mak19@gmail.com>
2021-10-15 09:40:02 +0800

    Update complexity_hurst.py
    I copied this function that works as-is, but I wonder how does it relate to our other functions... is there some time-embedding going on? can we expose and better select the delay range? @Tam-Pham @zen-juen

--- neurokit2/complexity/complexity_hurst.py
+++ neurokit2/complexity/complexity_hurst.py
@@ -5,7 +5,7 @@ import scipy.special
 from .fractal_dfa import _fractal_dfa_findwindows
 
 
-def complexity_hurst(signal, windows="default", corrected=False, show=False):
+def complexity_hurst(signal, windows="default", corrected=False, q=2, show=False):
     """Hurst Exponent (H)
 
     This function estimates the Hurst exponent via the standard rescaled range (R/S) approach, but other methods exist,
@@ -45,6 +45,7 @@ def complexity_hurst(signal, windows="default", corrected=False, show=False):
 
     References
     ----------
+    - Brandi, G., & Di Matteo, T. (2021). On the statistics of scaling exponents and the Multiscaling Value at Risk. The European Journal of Finance, 1-22.
     - https://github.com/CSchoel/nolds
 
     """
@@ -139,3 +140,43 @@ def _complexity_hurst_rs(signal, window):
         return np.nan
     # return mean of r/s along subsequence index
     return np.mean(r / s)
+
+
+def _complexity_hurst_generalized(signal, q=2):
+    """From https://github.com/PTRRupprecht/GenHurst/blob/master/genhurst.py"""
+
+    n = len(signal)
+    H = np.zeros((len(range(5, 20)), 1))
+    k = 0
+
+    for Tmax in range(5, 20):
+
+        x = np.arange(1, Tmax + 1, 1)
+        mcord = np.zeros((Tmax, 1))
+
+        for tt in range(1, Tmax + 1):
+            dV = signal[np.arange(tt, n, tt)] - signal[np.arange(tt, n, tt) - tt]
+            VV = signal[np.arange(tt, n + tt, tt) - tt]
+            N = len(dV) + 1
+            X = np.arange(1, N + 1, dtype=np.float64)
+            Y = VV
+            mx = np.sum(X) / N
+            SSxx = np.sum(X ** 2) - N * mx ** 2
+            my = np.sum(Y) / N
+            SSxy = np.sum(np.multiply(X, Y)) - N * mx * my
+            cc1 = SSxy / SSxx
+            cc2 = my - cc1 * mx
+            ddVd = dV - cc1
+            VVVd = VV - np.multiply(cc1, np.arange(1, N + 1, dtype=np.float64)) - cc2
+            mcord[tt - 1] = np.mean(np.abs(ddVd) ** q) / np.mean(np.abs(VVVd) ** q)
+
+        mx = np.mean(np.log10(x))
+        SSxx = np.sum(np.log10(x) ** 2) - Tmax * mx ** 2
+        my = np.mean(np.log10(mcord))
+        SSxy = np.sum(np.multiply(np.log10(x), np.transpose(np.log10(mcord)))) - Tmax * mx * my
+        H[k] = SSxy / SSxx
+        k = k + 1
+
+    mH = np.mean(H) / q
+
+    return mH
