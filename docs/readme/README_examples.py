67bd7110907d3776aab175a08b6ca67b0f205ca0
Zen <lauzenjuen@gmail.com>
2021-11-01 13:27:00 +0800

    ok wait nvm

--- docs/readme/README_examples.py
+++ docs/readme/README_examples.py
@@ -5,7 +5,6 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib.cm
 import neurokit2 as nk
-import PyEMD
 
 # setup matplotlib with Agg to run on server
 matplotlib.use('Agg')
