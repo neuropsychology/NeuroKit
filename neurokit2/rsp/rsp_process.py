474d03bafece9e2e8d550a867eba29acea17cafc
An Shu Te <teanshu97@gmail.com>
2022-04-13 14:16:35 +0800

    update rsp_process

--- neurokit2/rsp/rsp_process.py
+++ neurokit2/rsp/rsp_process.py
@@ -9,15 +9,13 @@ from .rsp_phase import rsp_phase
 
 
 def rsp_process(rsp_signal, sampling_rate=1000, method="khodadad2018"):
-    """Process a respiration (RSP) signal.
+    """**Process a respiration (RSP) signal**
 
     Convenience function that automatically processes a respiration signal with one of the following methods:
 
-    - `Khodadad et al. (2018)
-    <https://iopscience.iop.org/article/10.1088/1361-6579/aad7e6/meta>`_
+    * `Khodadad et al. (2018) <https://iopscience.iop.org/article/10.1088/1361-6579/aad7e6/meta>`_
 
-    - `BioSPPy
-    <https://github.com/PIA-Group/BioSPPy/blob/master/biosppy/signals/resp.py>`_
+    * `BioSPPy <https://github.com/PIA-Group/BioSPPy/blob/master/biosppy/signals/resp.py>`_
 
     Parameters
     ----------
@@ -31,21 +29,23 @@ def rsp_process(rsp_signal, sampling_rate=1000, method="khodadad2018"):
     Returns
     -------
     signals : DataFrame
-        A DataFrame of same length as `rsp_signal` containing the following
-        columns:
-        - *"RSP_Raw"*: the raw signal.
-        - *"RSP_Clean"*: the cleaned signal.
-        - *"RSP_Peaks"*: the respiratory peaks (exhalation onsets) marked as "1" in a list of zeros.
-        - *"RSP_Troughs"*: the respiratory troughs (inhalation onsets) marked as "1" in a list of zeros.
-        - *"RSP_Rate"*: breathing rate interpolated between inhalation peaks.
-        - *"RSP_Amplitude"*: breathing amplitude interpolated between inhalation peaks.
-        - *"RSP_Phase"*: breathing phase, marked by "1" for inspiration and "0" for expiration.
-        - *"RSP_PhaseCompletion"*: breathing phase completion, expressed in percentage (from 0 to 1),
-        representing the stage of the current respiratory phase.
+        A DataFrame of same length as `rsp_signal` containing the following columns:
+
+        * **"RSP_Raw"**: the raw signal.
+        * **"RSP_Clean"**: the cleaned signal.
+        * **"RSP_Peaks"**: the respiratory peaks (exhalation onsets) marked as "1" in a list of
+          zeros.
+        * **"RSP_Troughs"**: the respiratory troughs (inhalation onsets) marked as "1" in a list of
+          zeros.
+        * **"RSP_Rate"**: breathing rate interpolated between inhalation peaks.
+        * **"RSP_Amplitude"**: breathing amplitude interpolated between inhalation peaks.
+        * **"RSP_Phase"**: breathing phase, marked by "1" for inspiration and "0" for expiration.
+        * **"RSP_PhaseCompletion"**: breathing phase completion, expressed in percentage (from 0 to
+          1), representing the stage of the current respiratory phase.
     info : dict
         A dictionary containing the samples at which inhalation peaks and exhalation troughs occur,
-        accessible with the keys "RSP_Peaks", and "RSP_Troughs" respectively,
-        as well as the signals' sampling rate.
+        accessible with the keys "RSP_Peaks", and "RSP_Troughs" respectively, as well as the
+        signals' sampling rate.
 
     See Also
     --------
@@ -53,12 +53,17 @@ def rsp_process(rsp_signal, sampling_rate=1000, method="khodadad2018"):
 
     Examples
     --------
-    >>> import neurokit2 as nk
-    >>>
-    >>> rsp = nk.rsp_simulate(duration=90, respiratory_rate=15)
-    >>> signals, info = nk.rsp_process(rsp, sampling_rate=1000)
-    >>> fig = nk.rsp_plot(signals)
-    >>> fig #doctest: +SKIP
+    .. ipython:: python
+
+      import neurokit2 as nk
+
+      rsp = nk.rsp_simulate(duration=90, respiratory_rate=15)
+      signals, info = nk.rsp_process(rsp, sampling_rate=1000)
+      @savefig p_rsp_process_1.png scale=100%
+      fig = nk.rsp_plot(signals)
+      fig
+      @suppress
+      plt.close()
 
     """
     # Sanitize input
