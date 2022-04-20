90182e3ebccf1a881bde25c95711486b7fe9a77f
Dominique Makowski <dom.mak19@gmail.com>
2022-04-20 13:14:21 +0800

    ignore examples for now
    hopefully will get fixed once https://github.com/executablebooks/MyST-NB/pull/380 is merged

--- docs_wip/conf.py
+++ docs_wip/conf.py
@@ -74,7 +74,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build"]
+exclude_patterns = ["_build", "examples"]
 
 # Ignore duplicated sections warning
 suppress_warnings = ["epub.duplicated_toc_entry"]
