3b6052f1a59ec9df9902f6a20986375dd9861e63
Dominique Makowski <dom.mak19@gmail.com>
2022-04-22 10:09:48 +0800

    [feature]: add 'show' option to fractal_petrosian

--- neurokit2/complexity/fractal_petrosian.py
+++ neurokit2/complexity/fractal_petrosian.py
@@ -1,16 +1,29 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 from ..signal.signal_binarize import _signal_binarize_threshold
 
 
-def fractal_petrosian(signal, method="C"):
+def fractal_petrosian(signal, method="C", show=False):
     """**Petrosian fractal dimension (PFD)**
 
     Petrosian proposed a fast method to estimate the fractal dimension of a finite sequence, which
     converts the data to binary sequence before estimating the fractal dimension from time series.
     Several variations of the algorithm exist (e.g., 'A', 'B', 'C' or 'D'), primarily differing in
     the way the binary sequence is created.
+
+    * **Method 'A'** binarizes the signal by higher vs. lower values as compated to the signal's
+      mean.
+    * **Method 'B'** uses values that are within the mean +/- 1 SD band vs. values that are outside
+      this band.
+    * **Method 'C'** computes the difference between consecutive samples and binarizes depending on
+      their sign.
+    * **Method 'D'** forms separates consecutive samples that exceed 1 signal's SD from the others
+      smaller changes.
+
+    The algorithm assumes the signal is periodic (without a linear trend). Linear detrending might
+    be useful to eliminate linear trends (see :func:`.signal_detrend`).
 
     See Also
     --------
@@ -21,11 +34,9 @@ def fractal_petrosian(signal, method="C"):
     signal : Union[list, np.array, pd.Series]
         The signal (i.e., a time series) in the form of a vector of values.
     method : str
-        Can be 'A', 'B', 'C' or 'D'. Method 'A' binarizes the signal by higher vs. lower values as
-        compated to the signal's mean. Method 'B' uses values that are within the mean +/- 1 SD band
-        vs. values that are outside this band. Method 'C' computes the difference between
-        consecutive samples and binarizes depending on their sign. Method 'D' forms separates
-        consecutive samples that exceed 1 signal's SD from the others smaller changes.
+        Can be 'A', 'B', 'C' or 'D'.
+    show : bool
+        If True, will show the binarizion of the signal.
 
     Returns
     -------
@@ -41,16 +52,51 @@ def fractal_petrosian(signal, method="C"):
 
       import neurokit2 as nk
 
-      signal = nk.signal_simulate(duration=2, frequency=5)
+      signal = nk.signal_simulate(duration=2, frequency=[5, 12])
 
-      pfd, info = nk.fractal_petrosian(signal, method = "A")
+      @savefig p_fractal_petrosian1.png scale=100%
+      pfd, info = nk.fractal_petrosian(signal, method = "A", show=True)
+      @suppress
+      plt.close()
+
+    .. ipython:: python
+
       pfd
       info
-      pfd, info = nk.fractal_petrosian(signal, method = "B")
+
+    .. ipython:: python
+
+      @savefig p_fractal_petrosian2.png scale=100%
+      pfd, info = nk.fractal_petrosian(signal, method = "B", show=True)
+      @suppress
+      plt.close()
+
+    .. ipython:: python
+
       pfd
-      pfd, info = nk.fractal_petrosian(signal, method = "C")
+
+    .. ipython:: python
+
+      @savefig p_fractal_petrosian3.png scale=100%
+      pfd, info = nk.fractal_petrosian(signal, method = "C", show=True)
+      @suppress
+      plt.close()
+
+    .. ipython:: python
+
       pfd
-      pfd, info = nk.fractal_petrosian(signal, method = "D")
+
+    .. ipython:: python
+
+      signal = nk.signal_simulate(duration=2, frequency=[5], noise = 0.1)
+
+      @savefig p_fractal_petrosian4.png scale=100%
+      pfd, info = nk.fractal_petrosian(signal, method = "D", show=True)
+      @suppress
+      plt.close()
+
+    .. ipython:: python
+
       pfd
 
     References
@@ -71,24 +117,37 @@ def fractal_petrosian(signal, method="C"):
     # Binarize the sequence
     if method == "A":
         n_inversions = _signal_binarize_threshold(signal, threshold="mean").sum()
+        if show is True:
+            df = pd.DataFrame({"A": signal, "B": signal})
+            df["A"][df["A"] > np.nanmean(signal)] = np.nan
+            df["B"][df["B"] <= np.nanmean(signal)] = np.nan
+            df.plot()
+            plt.axhline(y=np.nanmean(signal), color="r", linestyle="dotted")
     elif method == "B":
-        range = (
-            np.nanmean(signal) - np.nanstd(signal, ddof=1),
-            np.nanmean(signal) + np.nanstd(signal, ddof=1),
-        )
-        n_inversions = np.logical_or(signal < range[0], signal > range[1]).sum()
-    elif method == "C":
-        # Method 1 (antropy and https://stackoverflow.com/a/29674950/10581531)
-        n_inversions = np.diff(np.signbit(signal)).sum()
-
-        # # Method 2 (https://github.com/gilestrolab/pyrem/blob/master/src/pyrem/univariate.py#L169)
-        # diff = np.diff(signal)
-        # prod = diff[1:-1] * diff[0:-2]  # x[i] * x[i-1] for i in t0 -> tmax
-        # n_inversions = np.sum(prod < 0)
-    elif method == "D":
+        m = np.nanmean(signal)
         sd = np.nanstd(signal, ddof=1)
-        diff = np.abs(np.diff(signal))
-        n_inversions = (diff > sd).sum()
+        n_inversions = np.logical_or(signal < m - sd, signal > m + sd).sum()
+        if show is True:
+            df = pd.DataFrame({"A": signal, "B": signal})
+            df["A"][np.logical_or(signal < m - sd, signal > m + sd)] = np.nan
+            df["B"][~np.isnan(df["A"])] = np.nan
+            df.plot()
+            plt.axhline(y=m - sd, color="r", linestyle="dotted")
+            plt.axhline(y=m + sd, color="r", linestyle="dotted")
+    elif method == "C":
+        n_inversions = np.diff(np.signbit(np.diff(signal))).sum()
+        if show is True:
+            df = pd.DataFrame({"A": signal, "B": signal})
+            df["A"][np.insert(np.signbit(np.diff(signal)), 0, False)] = np.nan
+            df["B"][~np.isnan(df["A"])] = np.nan
+            df.plot()
+
+    elif method == "D":
+        n_inversions = (np.abs(np.diff(signal)) > np.nanstd(signal, ddof=1)).sum()
+        if show is True:
+            where = np.where(np.abs(np.diff(signal)) > np.nanstd(signal, ddof=1))[0]
+            plt.plot(signal, zorder=1)
+            plt.scatter(where, signal[where], color="orange", label="Inversion", zorder=2)
     else:
         raise ValueError(
             "method must be one of 'A', 'B', 'C' or 'D'. See the documentation for more information."
