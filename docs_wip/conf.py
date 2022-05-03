97a54e6cdf24c087b1a30070e3b0e33f294b3c97
Dominique Makowski <dom.mak19@gmail.com>
2022-05-03 18:10:59 +0800

    add ppg

--- docs_wip/conf.py
+++ docs_wip/conf.py
@@ -95,6 +95,7 @@ add_module_names = False  # If true, the current module name will be prepended t
 
 # -- Options for myst_nb ---------------------------------------------------
 nb_execution_mode = "force"
+nb_execution_raise_on_error = True
 
 # googleanalytics_id = "G-DVXSEGN5M9"
 
