b7494083b929546f9b18c7b1ea2b263eb2cde8b1
Dominique Makowski <dom.mak19@gmail.com>
2022-06-01 10:23:17 +0800

    rework

--- neurokit2/misc/find_outliers.py
+++ neurokit2/misc/find_outliers.py
@@ -4,20 +4,30 @@ import scipy
 from ..stats import standardize
 
 
-def find_outliers(data, exclude=0.05, side="both", method="standardize", **kwargs):
-    """Identify outliers (abnormal values)
+def find_outliers(data, exclude=2, side="both", method="sd", **kwargs):
+    """**Identify outliers (abnormal values)**
 
-    Extreme values identification.
+    Extreme values identification using different methods, such as:
+
+    * **sd**: Data is :func:`standardized <.standardize>`, i.e., centered and
+      scaled, and absolute value beyond a certain SD threshold are considered as outliers.
+    * **norm**: Extreme values identified using theoretical percentiles to identify outliers
+      beyond a certain theoretical percentile (assuming the data comes from a normal distribution).
+      For example, with this method, ``exclude=0.025`` (one-sided) corresponds to the 2.5% lower
+      bound of the normal distribution, which corresponds to approx. -1.96 SD. This method is
+      related to the **SD** one, but instead of specifying the threshold in SDs, it is specified in
+      percentiles.
+    * **percentile**: Extreme values identified using percentiles.
 
     Parameters
     ----------
     data : list or ndarray
         Data array
     exclude : int, float
-        Proportion of extreme observations to be excluded for "standardize" method.
+        Amount of outliers to detect (depends on the chosen method).
     side: str
-        Can be "both", "left" or "right". If side="both", the extreme
-        values identified on each side will be marked as outliers.
+        Can be ``"both"``, ``"left"`` or ``"right"``. If ``exclude=0.05`` and ``side="both"`` and
+        ``method="norm"``, 2.5% of extreme observation of each side will be marked as outliers.
     method: str
         Can be "standardize" or "percentile". The default is "standardize".
     **kwargs : optional
@@ -27,110 +37,82 @@ def find_outliers(data, exclude=0.05, side="both", method="standardize", **kwarg
     Returns
     ----------
     outliers : ndarray
-        A list of True/False with True being the outliers.
+        A boolean vector of with ``True`` being the outliers.
+
+    See Also
+    ----------
+    .standardize
 
     Example
     ----------
-    >>> import neurokit2 as nk
-    >>> outliers = nk.find_outliers([1, 2, 1, 5, 666, 4, 1 ,3, 5])
-    >>> outliers
-    array([False, False, False, False,  True, False, False, False, False])
+    .. ipython:: python
+
+      import neurokit2 as nk
+
+      data = [-12, 2, 1, 3, 66.6, 2, 1, 3, 2, -42, 2, 4, 1, 12]
+
+      # Outliers beyond 2 SD of the mean
+      outliers = nk.find_outliers(data, exclude=2, side="both", method="sd")
+      np.where(outliers)[0]
+
+      # Outliers beyond 1 MAD of the Median on one side
+      outliers = nk.find_outliers(data, exclude=1, side="left", method="sd", robust=True)
+      np.where(outliers)[0]
+
+      # 2.5% theoretical percentiles on each side
+      outliers = nk.find_outliers(data, exclude=0.05, method="norm")
+      np.where(outliers)[0]
+
+      # Outliers are beyond interquartile range
+      outliers = nk.find_outliers(data, exclude=(0.25, 0.75), method="percentile")
+      np.where(outliers)[0]
+
+      # Outliers are beyond interdecile range
+      outliers = nk.find_outliers(data, exclude=(0.1, 0.9), method="percentile")
+      np.where(outliers)[0]
 
     """
+    # Sanity checks
     if side not in ["both", "left", "right"]:
         raise ValueError("side must be 'both', 'left' or 'right'.")
 
-    if method not in ["standardize", "percentile"]:
+    method = method.lower()
+    if method not in ["standardize", "z", "sd", "percentile", "norm"]:
         raise ValueError("method must be 'standardize' or 'percentile'.")
 
-    if method == "standardize":
-        outliers = _find_outliers_standardize(data, exclude=exclude, side=side, **kwargs)
-    elif method == "percentile":
-        outliers = _find_outliers_percentile(data, side=side, **kwargs)
-    return np.array(outliers)
+    # Force array
+    data = np.array(data)
 
+    # Find thresholds
+    if method in ["percentile"]:
+        if isinstance(exclude, (list, tuple, np.ndarray)):
+            right = np.percentile(data, exclude[1] * 100)
+            left = np.percentile(data, exclude[0] * 100)
+        else:
+            right = np.percentile(data, (1 - (exclude[1] / 2)) * 100)
+            left = np.percentile(data, (exclude[0] / 2) * 100)
 
-def _find_outliers_standardize(data, exclude=0.05, side="both", **kwargs):
-    """Identify outliers (abnormal values) with standardization.
+    elif method in ["sd"]:
+        if isinstance(exclude, (list, tuple, np.ndarray)):
+            right = exclude[1]
+            left = exclude[0]
+        else:
+            right = exclude
+            left = -right
+    else:
+        if side == "both":
+            exclude = exclude / 2
+        right = scipy.stats.norm.ppf(1 - exclude)
+        left = -right
 
-    Extreme values identification using standardization, i.e., centering and
-    scaling.
+    if method in ["standardize", "z", "sd", "norm"]:
+        data = np.array(standardize(data, **kwargs))
 
-    Parameters
-    ----------
-    data : list or ndarray
-        Data array
-    exclude : int, float
-        Proportion of extreme observation to be excluded.
-    side: str
-        Can be "both", "left" or "right". If exclude=0.05 and side="both",
-        2.5% of extreme observations of each side will be marked as outliers.
-    **kwargs : optional
-    Other arguments to be passed to :func:`standardize`.
-
-    Returns
-    ----------
-    outliers : ndarray
-        A list of True/False with True being the outliers.
-
-    """
-    if side not in ["both", "left", "right"]:
-        raise ValueError("side must be 'both', 'left' or 'right'.")
-
-    z = np.array(standardize(data, **kwargs))
     if side == "both":
-        outliers = abs(z) > scipy.stats.norm.ppf(1 - (exclude / 2))
+        outliers = (data < left) | (data > right)
     elif side == "left":
-        outliers = z < -scipy.stats.norm.ppf(1 - exclude)
+        outliers = data < left
     elif side == "right":
-        outliers = z > scipy.stats.norm.ppf(1 - exclude)
-    return np.array(outliers)
+        outliers = data > right
 
-
-def _find_outliers_percentile(
-    data, side="both", percentile_threshold=(0.25, 0.75), percentile_range=(0.25, 0.75), multiplier_range=1.5
-):
-    """Identify outliers (abnormal values) with percentiles.
-
-    Extreme values identification using percentiles.
-
-    Parameters
-    ----------
-    data : list or ndarray
-        Data array
-    side: str
-        Can be "both", "left" or "right". If side="both", the extreme
-        values identified on each side will be marked as outliers.
-    percentile_threshold : tuple
-        Percentiles for the left and right threshold.
-    percentile_range : tuple
-        Percentiles for the inter-percentile range. (0.25, 0.75) is the
-        interquartile range; (0.10, 0.90) is the interdecile range.
-    multiplier_range : int, float
-        Multiplier for the inter-percentile range. If side="both" and
-        multiplier_range=1.5, observations more than 1.5 inter-percentile
-        ranges below the left percentile or above the right percentile will
-        be marked as outliers.
-
-    Returns
-    ----------
-    outliers : ndarray
-        A list of True/False with True being the outliers.
-
-    """
-    if side not in ["both", "left", "right"]:
-        raise ValueError("side must be 'both', 'left' or 'right'.")
-
-    if isinstance(data, list):
-        data = np.array(data)
-
-    inter_percentile_range = np.percentile(data, percentile_range[1]) - np.percentile(data, percentile_range[0])
-    left_threshold = np.percentile(data, percentile_threshold[0]) - multiplier_range * inter_percentile_range
-    right_threshold = np.percentile(data, percentile_threshold[1]) + multiplier_range * inter_percentile_range
-    if side == "both":
-        outliers = (data > left_threshold) & (data < right_threshold)
-    elif side == "left":
-        outliers = data > left_threshold
-    elif side == "right":
-        outliers = data < right_threshold
     return outliers
