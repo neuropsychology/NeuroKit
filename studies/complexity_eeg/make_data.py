ab6e5fb3f14be4a5458e9ecfde4671f5ee8b402f
Dominique Makowski <dom.mak19@gmail.com>
2022-06-14 17:18:06 +0800

    update

--- studies/complexity_eeg/make_data.py
+++ studies/complexity_eeg/make_data.py
@@ -14,37 +14,31 @@ datasets = [
     # "../../data/rs_eeg_texas/data/",  # Path to local TEXAS dataset
     "../../data/srm_restingstate_eeg/eeg/",  # Path to local SRM dataset
     "../../data/testretest_restingstate_eeg/eeg/",  # Path to local testrestest dataset
-    "C:/Dropbox/RECHERCHE/Studies/RestingStateComplexity/data/eeg_sg/",
-    "C:/Dropbox/RECHERCHE/Studies/RestingStateComplexity/data/eeg_fr/",
+    "../../data/rebel_eeg_restingstate_sg/eeg/",
+    "../../data/rebel_eeg_restingstate_fr/eeg/",
 ]
 
 
 # =============================================================================
 # Functions
 # =============================================================================
-# participant = "EEG_Cat_Study4_Resting_S1.bdf"
-# path = "../../data/rs_eeg_texas/data/"
 # channel = "Fp1"
-# method = "rosenstein1993"
 
 
 def optimize_delay(raw, channel="Fp1"):
     signal = raw.get_data(picks=channel)[0]
-    vals = np.unique(np.round(nk.expspace(1, 80, 60) / (1000 / raw.info["sfreq"]), 0)).astype(int)
+    vals = np.unique(np.round(np.linspace(1, 80, 80) / (1000 / raw.info["sfreq"]), 0)).astype(int)
     vals = vals[vals > 0]
-    rez_delay = pd.DataFrame()
-    for method in ["fraser1986", "mi2"]:
-        delay, out = nk.complexity_delay(signal, delay_max=vals, method=method)
+    delay, out = nk.complexity_delay(signal, delay_max=vals, method="fraser1986")
 
-        rez = pd.DataFrame({"Value": out["Values"], "Score": out["Scores"]})
-        rez["Method"] = out["Method"]
-        rez["Metric"] = out["Metric"]
-        # rez_delay["Algorithm"] = out["Algorithm"]
-        rez["Channel"] = channel
-        rez["Optimal"] = delay
-        rez["What"] = "Delay"
-        rez_delay = pd.concat([rez_delay, rez], axis=0)
-    return rez_delay
+    rez = pd.DataFrame({"Value": out["Values"], "Score": out["Scores"]})
+    rez["Method"] = out["Method"]
+    rez["Metric"] = out["Metric"]
+    # rez_delay["Algorithm"] = out["Algorithm"]
+    rez["Channel"] = channel
+    rez["Optimal"] = delay
+    rez["What"] = "Delay"
+    return rez
 
 
 def optimize_dimension(raw, channel="Fp1"):
@@ -52,7 +46,7 @@ def optimize_dimension(raw, channel="Fp1"):
     dim, out = nk.complexity_dimension(
         signal,
         dimension_max=5,
-        delay=np.round(30 / (1000 / raw.info["sfreq"]), 0).astype(int),
+        delay=np.round(27 / (1000 / raw.info["sfreq"]), 0).astype(int),
         method="afn",
     )
 
@@ -62,6 +56,31 @@ def optimize_dimension(raw, channel="Fp1"):
     rez["Optimal"] = dim
     rez["What"] = "Dimension"
     return rez
+
+
+def optimize_tolerance(raw, channel="Fp1"):
+    signal = raw.get_data(picks=channel)[0]
+    signal = nk.standardize(signal)
+    out = pd.DataFrame()
+    for method in ["maxApEn", "recurrence"]:
+
+        r, info = nk.complexity_tolerance(
+            signal,
+            delay=np.round(27 / (1000 / raw.info["sfreq"]), 0).astype(int),
+            dimension=5,
+            method=method,
+            r_range=np.linspace(0.002, 2, 10),
+            show=True,
+        )
+
+        rez = pd.DataFrame({"Value": info["Values"], "Score": info["Scores"]})
+        rez["Method"] = method
+        rez["Channel"] = channel
+        rez["Optimal"] = r
+        rez["What"] = "Dimension"
+        out = pd.concat([out, rez], axis=0)
+
+    return out
 
 
 def read_raw(path, participant):
@@ -92,13 +111,14 @@ def read_raw(path, participant):
         else:
             cond = "Eyes-Closed"
         raw = mne.io.read_raw_fif(path + participant, verbose=False, preload=True)
-    elif "RestingStateComplexity" in path:
-        pass
-        # os.listdir(path)
-        # dataset = "SRM"
-        # cond = "Eyes-Closed"
-        # sub = participant.split("_")[0]
-        # raw = mne.io.read_raw_fif(path + participant, verbose=False, preload=True)
+    elif "rebel_eeg_restingstate" in path:
+        if "_sg" in path:
+            dataset = "Resting-State (SG)"
+        else:
+            dataset = "Resting-State (FR)"
+        cond = "Eyes-Closed"
+        sub = participant
+        raw = mne.io.read_raw_fif(path + participant, verbose=False, preload=True)
     else:
         dataset = "Lemon"
         sub = participant.split("_")[0]
@@ -114,38 +134,76 @@ def read_raw(path, participant):
     return raw, sub, cond, orig_filtering, dataset
 
 
+def compute_complexity(raw, channel="Fp1"):
+    signal = raw.get_data(picks=channel)[0]
+    signal = nk.standardize(signal)
+
+    delay = np.round(27 / (1000 / raw.info["sfreq"]), 0).astype(int)
+    m = 5
+    r, _ = nk.complexity_tolerance(signal, dimension=m, method="NeuroKit")
+
+    rez = pd.DataFrame({"Channel": [channel]})
+    rez["SFD"], _ = nk.fractal_sevcik(signal)  # Change ShanEn D by SFD
+    rez["MSWPEn"], _ = nk.entropy_multiscale(
+        signal, scale="default", dimension=m, tolerance=r, method="MSWPEn"
+    )
+    rez["CWPEn"], _ = nk.entropy_permutation(
+        signal,
+        delay=delay,
+        dimension=m,
+        tolerance=r,
+        weighted=True,
+        conditional=True,
+    )
+    rez["AttEn"], _ = nk.entropy_attention(
+        signal,
+    )
+    rez["SVDEn"], _ = nk.entropy_svd(signal, delay=delay, dimension=m)
+    rez["Hjorth"], _ = nk.complexity_hjorth(signal)
+    rez["FDNLD"], _ = nk.fractal_nld(signal)
+
+    mfdfa, _ = nk.fractal_dfa(signal, multifractal=True)
+    rez["MFDFA_Width"] = mfdfa["Width"]
+    rez["MFDFA_Max"] = mfdfa["Max"]
+    rez["MFDFA_Mean"] = mfdfa["Mean"]
+    rez["MFDFA_Increment"] = mfdfa["Increment"]
+
+    return rez
+
+
 # =============================================================================
 # Delay
 # =============================================================================
-rez_delay = pd.DataFrame()
-# rez_delay = pd.read_csv("data_delay.csv")
+# rez_delay = pd.DataFrame()
 
-for path in datasets:
-    participants = os.listdir(path)
+# for path in datasets:
+#     participants = os.listdir(path)
 
-    for i, participant in enumerate(participants):
-        if i < 0 or i > 3:
-            continue
-        print(f"Participant n°{i} (path: {path})")
+#     for i, participant in enumerate(participants):
+#         if i < 0 or i > 3:
+#             continue
+#         print(f"Participant n°{i} (path: {path})")
 
-        raw, sub, cond, orig_filtering, dataset = read_raw(path, participant)
-        args = [{"raw": raw, "channel": ch} for ch in raw.pick_types(eeg=True).ch_names]
-        out = nk.parallel_run(optimize_delay, args, n_jobs=8, verbose=10)
-        out = pd.concat(out)
+#         raw, sub, cond, orig_filtering, dataset = read_raw(path, participant)
+#         args = [{"raw": raw, "channel": ch} for ch in raw.pick_types(eeg=True).ch_names]
+#         for i in args:
+#             optimize_delay(i["raw"], i["channel"])
+#         out = nk.parallel_run(optimize_delay, args, n_jobs=1, verbose=10)
+#         out = pd.concat(out)
 
-        out["Participant"] = sub
-        out["Condition"] = cond
-        out["Sampling_Rate"] = raw.info["sfreq"]
-        out["Lowpass"] = raw.info["lowpass"]
-        out["Original_Frequencies"] = orig_filtering
-        out["Duration"] = len(raw) / raw.info["sfreq"] / 60
-        out["Dataset"] = dataset
+#         out["Participant"] = sub
+#         out["Condition"] = cond
+#         out["Sampling_Rate"] = raw.info["sfreq"]
+#         out["Lowpass"] = raw.info["lowpass"]
+#         out["Original_Frequencies"] = orig_filtering
+#         out["Duration"] = len(raw) / raw.info["sfreq"] / 60
+#         out["Dataset"] = dataset
 
-        rez_delay = pd.concat([rez_delay, out], axis=0)
-        rez_delay.to_csv("data_delay.csv", index=False)
+#         rez_delay = pd.concat([rez_delay, out], axis=0)
+#         rez_delay.to_csv("data_delay.csv", index=False)
 
-print("===================")
-print("FINISHED.")
+# print("===================")
+# print("FINISHED.")
 
 # =============================================================================
 # Attractors
@@ -155,10 +213,14 @@ for path in datasets:
     participants = os.listdir(path)[2]
     raw, _, _, _, dataset = read_raw(path, participants)
     raw = raw.crop(tmin=30, tmax=90)
-    for channel in ["Fz", "Pz"]:
+    for channel in ["Cz", "Fz", "AFz", "Pz", "Oz"]:  # raw.ch_names
+        if channel not in raw.ch_names:
+            print(f"Channel: {channel}, Dataset: {dataset}")
+            continue
         signal = nk.standardize(raw.get_data(picks=channel)[0])
-        # nk.signal_psd(signal, sampling_rate=raw.info["sfreq"], max_frequency=100, show=True)
-        d = np.round(np.array([30]) / (1000 / raw.info["sfreq"]), 0).astype(int)
+        # if "testretest" in path:
+        #     nk.signal_psd(signal, sampling_rate=raw.info["sfreq"], max_frequency=100, show=True)
+        d = np.round(np.array([27]) / (1000 / raw.info["sfreq"]), 0).astype(int)
         for i, delay in enumerate(d):
             if delay == 0:
                 continue
@@ -168,7 +230,7 @@ for path in datasets:
             data["Sampling_Rate"] = raw.info["sfreq"]
             data["Delay"] = delay
             data["Delay_Type"] = i
-            data["Channel"] = channel
+            data["Channel"] = "Fz" if channel == "AFz" else channel
             data["Time"] = raw.times[0 : len(data)]
             attractors = pd.concat([attractors, data], axis=0)
 attractors.to_csv("data_attractor.csv", index=False)
@@ -222,5 +284,66 @@ for path in datasets:
         rez_dim = pd.concat([rez_dim, out], axis=0)
         rez_dim.to_csv("data_dimension.csv", index=False)
 
+print("===================")
+print("FINISHED.")
+
+
+# =============================================================================
+# Tolerance
+# =============================================================================
+rez_r = pd.DataFrame()
+
+for path in datasets:
+    participants = os.listdir(path)
+
+    for i, participant in enumerate(participants):
+        if i < 0 or i > 1:
+            continue
+        print(f"Participant n°{i} (path: {path})")
+
+        raw, sub, cond, orig_filtering, dataset = read_raw(path, participant)
+        args = [{"raw": raw, "channel": ch} for ch in raw.pick_types(eeg=True).ch_names]
+        out = nk.parallel_run(optimize_tolerance, args, n_jobs=8, verbose=10)
+        out = pd.concat(out)
+
+        out["Participant"] = sub
+        out["Condition"] = cond
+        out["Sampling_Rate"] = raw.info["sfreq"]
+        out["Lowpass"] = raw.info["lowpass"]
+        out["Original_Frequencies"] = orig_filtering
+        out["Duration"] = len(raw) / raw.info["sfreq"] / 60
+        out["Dataset"] = dataset
+
+        rez_r = pd.concat([rez_r, out], axis=0)
+        rez_r.to_csv("data_tolerance.csv", index=False)
+
+print("===================")
+print("FINISHED.")
+
+
+# =============================================================================
+# Clustering
+# =============================================================================
+rez_complexity = pd.DataFrame()
+
+for path in datasets:
+    participants = os.listdir(path)
+
+    for i, participant in enumerate(participants):
+        if i < 0 or i > 100:
+            continue
+        print(f"Participant n°{i} (path: {path})")
+
+        raw, sub, cond, orig_filtering, dataset = read_raw(path, participant)
+        args = [{"raw": raw, "channel": ch} for ch in raw.pick_types(eeg=True).ch_names]
+        out = nk.parallel_run(compute_complexity, args, n_jobs=1, verbose=10)
+        out = pd.concat(out)
+
+        out["Participant"] = sub
+        out["Condition"] = cond
+        out["Dataset"] = dataset
+
+        rez_complexity = pd.concat([rez_complexity, out], axis=0)
+        rez_complexity.to_csv("data_complexity.csv", index=False)
 print("===================")
 print("FINISHED.")
