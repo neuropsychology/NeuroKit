af874a63811463b93d14dfbf7474443082579662
Dominique Makowski <dom.mak19@gmail.com>
2022-06-29 14:45:05 +0800

    remove dep

--- docs/conf.py
+++ docs/conf.py
@@ -21,25 +21,54 @@
 import os
 import re
 import sys
+
 import mock
-from m2r import MdInclude
 from recommonmark.transform import AutoStructify
 
-
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Mock modules ---------------------------------------------
-MOCK_MODULES = ['scipy', 'scipy.signal', 'scipy.ndimage', 'scipy.stats', 'scipy.misc', 'scipy.interpolate', 'scipy.sparse', 'scipy.linalg',
-                'scipy.spatial', 'scipy.special', 'scipy.integrate', 'scipy.cluster', 'scipy.optimize', "scikit-learn",
-                'sklearn', 'sklearn.neighbors', 'sklearn.mixture', 'sklearn.datasets', 'sklearn.metrics', 'sklearn.metrics.pairwise', 'sklearn.decomposition',
-                'sklearn.cluster', 'sklearn.cross_validation', 'mne', 'bioread', 'cvxopt', 'pywt',
-                "pyrqa", "pyrqa.analysis_type", "pyrqa.computation", "pyrqa.image_generator", "pyrqa.metric", "pyrqa.neighbourhood", "pyrqa.settings", "pyrqa.time_series"]
+MOCK_MODULES = [
+    "scipy",
+    "scipy.signal",
+    "scipy.ndimage",
+    "scipy.stats",
+    "scipy.misc",
+    "scipy.interpolate",
+    "scipy.sparse",
+    "scipy.linalg",
+    "scipy.spatial",
+    "scipy.special",
+    "scipy.integrate",
+    "scipy.cluster",
+    "scipy.optimize",
+    "scikit-learn",
+    "sklearn",
+    "sklearn.neighbors",
+    "sklearn.mixture",
+    "sklearn.datasets",
+    "sklearn.metrics",
+    "sklearn.metrics.pairwise",
+    "sklearn.decomposition",
+    "sklearn.cluster",
+    "sklearn.cross_validation",
+    "mne",
+    "bioread",
+    "cvxopt",
+    "pywt",
+    "pyrqa",
+    "pyrqa.analysis_type",
+    "pyrqa.computation",
+    "pyrqa.image_generator",
+    "pyrqa.metric",
+    "pyrqa.neighbourhood",
+    "pyrqa.settings",
+    "pyrqa.time_series",
+]
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
-
-
 
 
 # -- General configuration ---------------------------------------------
@@ -51,22 +80,22 @@ for mod_name in MOCK_MODULES:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
-    'nbsphinx',
-    'sphinx_nbexamples',
-    'matplotlib.sphinxext.plot_directive',
-    'sphinx_copybutton',
-    'recommonmark',
-    'sphinx.ext.autosectionlabel'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+    "nbsphinx",
+    "sphinx_nbexamples",
+    "matplotlib.sphinxext.plot_directive",
+    "sphinx_copybutton",
+    "recommonmark",
+    "sphinx.ext.autosectionlabel",
 ]
 
 # matplotlib plot directive
@@ -78,11 +107,11 @@ plot_pre_code = """import numpy as np
 import pandas as pd"""
 
 # on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 # sphinx-nbexamples
-process_examples = not os.path.exists(os.path.join(os.path.dirname(__file__), 'examples'))
-not_document_data = 'sphinx_nbexamples.gallery_config'
+process_examples = not os.path.exists(os.path.join(os.path.dirname(__file__), "examples"))
+not_document_data = "sphinx_nbexamples.gallery_config"
 
 # Style autodoc
 napoleon_google_docstring = False
@@ -92,23 +121,26 @@ napoleon_use_ivar = False
 napoleon_use_rtype = False
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
 def find_author():
-    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format("__author__"), open('../neurokit2/__init__.py').read())
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format("__author__"),
+        open("../neurokit2/__init__.py").read(),
+    )
     return str(result.group(1))
 
 
-project = u'NeuroKit'
+project = u"NeuroKit"
 copyright = u"2020, Dominique Makowski"
 author = find_author()
 
@@ -119,7 +151,10 @@ author = find_author()
 #
 # The short X.Y version.
 def find_version():
-    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format("__version__"), open('../neurokit2/__init__.py').read())
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format("__version__"),
+        open("../neurokit2/__init__.py").read(),
+    )
     return result.group(1)
 
 
@@ -137,10 +172,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'monokai'  # 'default', 'monokai'
+pygments_style = "monokai"  # 'default', 'monokai'
 # nbsphinx_codecell_lexer = 'default'  # Doesn't do anything :/
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
@@ -152,23 +187,20 @@ html_show_sphinx = False
 # -- Options for HTML THEME: sphinx_rtd_theme -------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_favicon = "img/icon.ico"
 html_logo = "img/neurokit.png"
-html_static_path = ['_static']  # Folder that contain custom static files (e.g., CSS files)
+html_static_path = ["_static"]  # Folder that contain custom static files (e.g., CSS files)
 
 # Theme options are theme-specific and customize the look and feel of a theme further.
 # For a list of options available for each theme, see https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
-html_theme_options = {
-    'collapse_navigation': False  # Expandables entries
-}
-
+html_theme_options = {"collapse_navigation": False}  # Expandables entries
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'neurokit2doc'
+htmlhelp_basename = "neurokit2doc"
 
 
 # Bootstrap theme
@@ -193,29 +225,25 @@ htmlhelp_basename = 'neurokit2doc'
 
 
 # -- Options for LaTeX output ------------------------------------------
-pdf_title = u'NeuroKit2'
-author_field = u'Official Documentation'
+pdf_title = u"NeuroKit2"
+author_field = u"Official Documentation"
 
 latex_elements = {
-    'sphinxsetup': r"""
+    "sphinxsetup": r"""
         VerbatimColor={RGB}{38,50,56},
         verbatimwithframe=false,
         """
     # Background color of chunks
     # '
-
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -225,11 +253,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-        (master_doc,
-         'neurokit2.tex',
-         pdf_title,
-         author_field,
-         'manual'),
+    (master_doc, "neurokit2.tex", pdf_title, author_field, "manual"),
 ]
 
 
@@ -237,13 +261,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-        (master_doc,
-         'neurokit2',
-         pdf_title,
-         [author_field],
-         1)
-]
+man_pages = [(master_doc, "neurokit2", pdf_title, [author_field], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -252,34 +270,42 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc,
-     'neurokit2',
-     pdf_title,
-     author_field,
-     'neurokit2',
-     'The Python Toolbox for Neurophysiological Signal Processing.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "neurokit2",
+        pdf_title,
+        author_field,
+        "neurokit2",
+        "The Python Toolbox for Neurophysiological Signal Processing.",
+        "Miscellaneous",
+    ),
 ]
 
 
 # Other
 add_module_names = False  # so functions aren’t prepended with the name of the package/module
-add_function_parentheses = True  # to ensure that parentheses are added to the end of all function names
+add_function_parentheses = (
+    True  # to ensure that parentheses are added to the end of all function names
+)
 
 
 # -- Setup for recommonmark ---------------------------------------------
 def setup(app):
     # Use m2r only for mdinclude and recommonmark for everything else
     # https://github.com/readthedocs/recommonmark/issues/191#issuecomment-622369992
-    app.add_config_value('recommonmark_config', {
+    app.add_config_value(
+        "recommonmark_config",
+        {
             # 'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
+            "auto_toc_tree_section": "Contents",
+        },
+        True,
+    )
     app.add_transform(AutoStructify)
 
     # from m2r to make `mdinclude` work
-    app.add_config_value('no_underscore_emphasis', False, 'env')
-    app.add_config_value('m2r_parse_relative_links', False, 'env')
-    app.add_config_value('m2r_anonymous_references', False, 'env')
-    app.add_config_value('m2r_disable_inline_math', False, 'env')
-    app.add_directive('mdinclude', MdInclude)
+    app.add_config_value("no_underscore_emphasis", False, "env")
+    app.add_config_value("m2r_parse_relative_links", False, "env")
+    app.add_config_value("m2r_anonymous_references", False, "env")
+    app.add_config_value("m2r_disable_inline_math", False, "env")
+    app.add_directive("mdinclude", MdInclude)
