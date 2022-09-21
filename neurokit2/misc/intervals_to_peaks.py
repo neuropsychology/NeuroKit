6039ec5e8d1563d84f2015ba82262143cd9c123a
Danielle Benesch <dbenesch@eers.ca>
2022-09-21 11:06:29 -0400

    modify intervals_to_peaks to preserve given sampling rate and timepoints

--- neurokit2/misc/intervals_to_peaks.py
+++ neurokit2/misc/intervals_to_peaks.py
@@ -1,9 +1,9 @@
 import numpy as np
+from .find_successive_intervals import find_successive_intervals
 
 
-def intervals_to_peaks(intervals):
-    """Convenience function to convert intervals to peaks,
-    such as from R-R intervals to R-peaks of an ECG signal.
+def intervals_to_peaks(intervals, intervals_time=None, sampling_rate=1000):
+    """Convenience function to convert intervals to peaks, such as from R-R intervals to R-peaks of an ECG signal.
 
     This can be useful if you do not have raw peak indices and have only
     interval data such as breath-to-breath (BBI) or rpeak-to-rpeak (RRI) intervals.
@@ -11,7 +11,11 @@ def intervals_to_peaks(intervals):
     Parameters
     ----------
     intervals : list or array
-        List or numpy array of intervals.
+        List or numpy array of intervals, in milliseconds.
+    intervals_time : list or array, optional
+        List or numpy array of timestamps corresponding to intervals, in seconds.
+    sampling_rate : int, optional
+        Sampling rate (Hz) of the continuous cardiac signal in which the peaks occur.
 
     Returns
     -------
@@ -33,6 +37,25 @@ def intervals_to_peaks(intervals):
       hrv_indices
 
     """
-    peaks = np.append([0], np.cumsum(intervals))
+    if intervals is None:
+        return None
 
-    return np.array([int(i) for i in peaks])
+    if intervals_time is None:
+        intervals_time = np.nancumsum(intervals) / 1000
+
+    intervals_time = intervals_time[np.isfinite(intervals)]
+    intervals = intervals[np.isfinite(intervals)]
+
+    non_successive_indices = np.arange(1, len(intervals_time))[np.invert(
+        find_successive_intervals(intervals, intervals_time))]
+
+    to_insert_indices = np.concatenate(
+        (np.array([0]), non_successive_indices))
+
+    times_to_insert = intervals_time[to_insert_indices] - \
+        intervals[to_insert_indices]/1000
+
+    peaks_time = np.sort(np.concatenate((intervals_time, times_to_insert)))
+    peaks = peaks_time*sampling_rate
+
+    return np.array([int(np.round(i)) for i in peaks])
