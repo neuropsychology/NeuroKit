b0a7c59945cd6ae73dca4543ebe24d2e41db3bf1
Danielle Benesch <dbenesch@eers.ca>
2022-09-27 10:10:49 -0400

    move intervals_successive to hrv_utils.py

--- neurokit2/hrv/hrv_utils.py
+++ neurokit2/hrv/hrv_utils.py
@@ -4,7 +4,7 @@ from warnings import warn
 import numpy as np
 import pandas as pd
 
-from ..misc import NeuroKitWarning, intervals_successive, intervals_to_peaks
+from ..misc import NeuroKitWarning, intervals_to_peaks
 from ..signal import signal_interpolate
 
 
@@ -62,13 +62,13 @@ def _hrv_sanitize_rri(rri, rri_time=None):
         rri_time = np.array(rri_time)
 
         # Confirm that timestamps are in seconds
-        successive_intervals = intervals_successive(rri, intervals_time=rri_time)
+        successive_intervals = _intervals_successive(rri, intervals_time=rri_time)
 
         if np.all(successive_intervals) is False:
             # If none of the differences between timestamps match
             # the length of the R-R intervals in seconds,
             # try converting milliseconds to seconds
-            converted_successive_intervals = intervals_successive(
+            converted_successive_intervals = _intervals_successive(
                 rri, intervals_time=rri_time / 1000
             )
 
@@ -205,3 +205,67 @@ def _hrv_sanitize_peaks(peaks):
                 )
 
     return peaks
+
+
+def _intervals_successive(intervals, intervals_time=None, thresh_unequal=2, n_diff=1):
+    """Identify successive intervals.
+
+    Identification of intervals that are consecutive
+    (e.g. in case of missing data).
+
+    Parameters
+    ----------
+    intervals : list or ndarray
+        Intervals, e.g. breath-to-breath (BBI) or rpeak-to-rpeak (RRI)
+    intervals_time : list or ndarray, optional
+        Time points corresponding to intervals, in seconds.
+    thresh_unequal : int or float, optional
+        Threshold at which the difference between time points is considered to
+        be unequal to the interval, in milliseconds.
+    n_diff: int, optional
+        The number of times values are differenced.
+        Can be used to check which values are valid for the n-th difference
+        assuming successive intervals.
+
+    Returns
+    ----------
+    array
+        An array of True/False with True being the successive intervals.
+
+    Examples
+    ----------
+    .. ipython:: python
+
+      import neurokit2 as nk
+
+      rri = [400, 500, 700, 800, 900]
+      rri_time = [0.7,  1.2, 2.5, 3.3, 4.2]
+
+      successive_intervals = nk.intervals_successive(rri, rri_time)
+      successive_intervals
+
+      rri = [400, 500, np.nan, 700, 800, 900]
+      successive_intervals = nk.intervals_successive(rri)
+      successive_intervals
+
+    """
+
+    # Convert to numpy array
+    intervals = np.array(intervals)
+
+    if intervals_time is None:
+        intervals_time = np.nancumsum(intervals / 1000)
+    else:
+        intervals_time = np.array(intervals_time)
+
+    intervals_time[np.isnan(intervals)] = np.nan
+
+    diff_intervals_time_ms = np.diff(intervals_time, n=n_diff) * 1000
+
+    abs_error_intervals_ref_time = abs(
+        diff_intervals_time_ms - np.diff(intervals[1:], n=n_diff - 1)
+    )
+
+    successive_intervals = abs_error_intervals_ref_time <= thresh_unequal
+
+    return np.array(successive_intervals)
