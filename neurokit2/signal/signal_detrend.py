29f89f19ff4c31998eba0bc74f1568ccac81fe09
danibene <34680344+danibene@users.noreply.github.com>
2022-11-14 20:31:13 -0500

    Update neurokit2/signal/signal_detrend.py

--- neurokit2/signal/signal_detrend.py
+++ neurokit2/signal/signal_detrend.py
@@ -45,7 +45,7 @@ def signal_detrend(
         Only used if ``method`` is "locreg". The detrending ``window`` should correspond to the
         1 divided by the desired low-frequency band to remove
         (``window = 1 / detrend_frequency``)
-        For instance, to remove frequencies below ``0.67Hz`` the window should be ``1500``
+        For instance, to remove frequencies below ``0.67Hz`` the window should be ``1.5``
         (``1000 / 0.67 = 1500``).
     stepsize : float
         Only used if ``method`` is ``"locreg"``.
