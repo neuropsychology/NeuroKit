2302082f6997e3c9365afc2dc18dbef959b312a8
danibene <34680344+danibene@users.noreply.github.com>
2023-08-11 08:24:05 -0400

    rename extracted folder to the desired name

--- neurokit2/data/database.py
+++ neurokit2/data/database.py
@@ -1,9 +1,8 @@
 import requests
 import zipfile
 from pathlib import Path
-from urllib.parse import urlsplit
 
-def download_zip(url, destination_path=None):
+def download_zip(url, destination_path):
     """Download a ZIP file from a URL and extract it to a destination directory.
     
     Parameters:
@@ -11,19 +10,13 @@ def download_zip(url, destination_path=None):
     url : str
         The URL of the ZIP file to download.
     destination_path : str, Path
-        The path to which the ZIP file will be extracted. If None, the folder name will be taken
-        from the last part of the URL path and downloaded to the current working directory.
+        The path to which the ZIP file will be extracted.
 
     Returns:
     --------
     bool
         True if the ZIP file was downloaded and extracted successfully, False otherwise.
     """
-    if destination_path is None:
-        # Name the ZIP file to be downloaded after the last part of the URL path
-        url_parts = urlsplit(url)
-        destination_path = Path(url_parts.path).name
-    
     # Ensure that the destination path is a Path object ending with ".zip"
     zip_filepath = Path(destination_path)
     if zip_filepath.suffix != ".zip":
@@ -42,7 +35,15 @@ def download_zip(url, destination_path=None):
 
         # Extract the ZIP file
         with zipfile.ZipFile(zip_filepath, "r") as zip_ref:
-            zip_ref.extractall(destination_directory)
+          extracted_folder_name = zip_ref.namelist()[0]
+            
+          # Extract the contents
+          zip_ref.extractall(destination_directory)
+          
+          # Rename the extracted folder to the desired name
+          extracted_folder_path = destination_directory / extracted_folder_name
+          new_folder_path = destination_directory / Path(destination_path).name
+          extracted_folder_path.rename(new_folder_path)
 
         # Clean up by removing the downloaded ZIP file
         zip_filepath.unlink()
