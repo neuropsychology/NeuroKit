41840d64f1a2bb6212bd0a7febfe52e563d498c2
danibene <34680344+danibene@users.noreply.github.com>
2023-08-11 07:54:58 -0400

    allow for destination_path as optional arg

--- neurokit2/data/database.py
+++ neurokit2/data/database.py
@@ -3,33 +3,37 @@ import zipfile
 from pathlib import Path
 from urllib.parse import urlsplit
 
-def download_zip(url, destination_directory, zip_filename=None):
+def download_zip(url, destination_path=None):
     """Download a ZIP file from a URL and extract it to a destination directory.
     
     Parameters:
     -----------
     url : str
         The URL of the ZIP file to download.
-    destination_directory : str, Path
-        The directory to which the ZIP file will be extracted.
-    zip_filename : str, optional
-        The name of the ZIP file to download. If None, the name will be taken from the last part of the URL path.
+    destination_path : str, Path
+        The path to which the ZIP file will be extracted. If None, the folder name will be taken
+        from the last part of the URL path and downloaded to the current working directory.
 
     Returns:
     --------
     bool
         True if the ZIP file was downloaded and extracted successfully, False otherwise.
     """
-    # Create the destination directory if it does not exist
-    Path(destination_directory).mkdir(parents=True, exist_ok=True)
-
-    if zip_filename is None:
+    if destination_path is None:
         # Name the ZIP file to be downloaded after the last part of the URL path
         url_parts = urlsplit(url)
-        zip_filename = Path(url_parts.path).name
+        destination_path = Path(url_parts.path).name
+    
+    # Ensure that the destination path is a Path object ending with ".zip"
+    zip_filepath = Path(destination_path)
+    if zip_filepath.suffix != ".zip":
+        zip_filepath = Path(zip_filepath.parent, zip_filepath.name + ".zip")
+
+    # Create the destination directory if it does not exist
+    destination_directory = Path(destination_path).parent
+    Path(destination_directory).mkdir(parents=True, exist_ok=True)
 
     # Download the ZIP file
-    zip_filepath = Path(destination_directory) / zip_filename
     response = requests.get(url)
 
     if response.status_code == 200:
