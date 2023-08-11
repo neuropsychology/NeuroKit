50340dad814c03b9dacdbdb7bce58298ee493d5a
danibene <34680344+danibene@users.noreply.github.com>
2023-08-11 07:13:39 -0400

    update init

--- neurokit2/data/__init__.py
+++ neurokit2/data/__init__.py
@@ -5,5 +5,6 @@ from .read_acqknowledge import read_acqknowledge
 from .read_bitalino import read_bitalino
 from .read_video import read_video
 from .write_csv import write_csv
+from .database import download_zip
 
-__all__ = ["read_acqknowledge", "read_bitalino", "read_video", "data", "write_csv"]
+__all__ = ["read_acqknowledge", "read_bitalino", "read_video", "data", "write_csv", "download_zip"]
