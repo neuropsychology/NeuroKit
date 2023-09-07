4d5d7e2fd0d124baeb1b35c3c4e371861990be68
danibene <34680344+danibene@users.noreply.github.com>
2023-09-07 17:06:53 -0400

    add missing parentheses

--- neurokit2/ecg/ecg_phase.py
+++ neurokit2/ecg/ecg_phase.py
@@ -124,7 +124,7 @@ def ecg_phase(ecg_cleaned, rpeaks=None, delineate_info=None, sampling_rate=None)
         -1
     ]  # Avoid filling beyond the last peak/trough
     ventricular[0:last_element] = (
-        pd.Series(ventricular).ffill.values[0:last_element]
+        pd.Series(ventricular).ffill().values[0:last_element]
     )
 
     # Ventricular Phase Completion
