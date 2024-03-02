71cecb287b23ebdbcb406234eb4134ffa7a46678
danibene <34680344+danibene@users.noreply.github.com>
2024-03-01 19:49:40 -0500

    fix order of inputs to loc

--- neurokit2/emg/emg_activation.py
+++ neurokit2/emg/emg_activation.py
@@ -231,11 +231,11 @@ def emg_activation(
 
     # Modify output produced by signal_formatpeaks.
     for x in range(len(emg_amplitude)):
-        if df_activity.loc["EMG_Activity", x] != 0:
+        if df_activity.loc[x, "EMG_Activity"] != 0:
             if df_activity.index[x] == df_activity.index.get_loc(x):
-                df_activity.loc["EMG_Activity", x] = 1
+                df_activity.loc[x, "EMG_Activity"] = 1
             else:
-                df_activity.loc["EMG_Activity", x] = 0
+                df_activity.loc[x, "EMG_Activity"] = 0
         if df_offsets["EMG_Offsets"][x] != 0:
             if df_offsets.index[x] == df_offsets.index.get_loc(x):
                 df_offsets["EMG_Offsets"][x] = 1
