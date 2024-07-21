ed728a82d686eb5315f70612b00e76b7e8c4c9b2
Johannes Günter Herforth <johannes.herforth@uni.lu>
2024-07-21 16:07:50 +0200

    conf.py: remove redundant options

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,7 @@ html_theme_options = {
     "use_issues_button": True,
     "path_to_docs": "docs/",
     "use_edit_page_button": True,
-    "logo_only": True,
+    # "logo_only": True,
     "show_toc_level": 1,
     "navigation_with_keys": False,
 }
@@ -140,4 +140,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
