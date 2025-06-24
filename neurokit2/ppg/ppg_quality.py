fdf4527680e01b48c6d2f1936964bdab80670d80
Peter H Charlton <peterhcharlton@gmail.com>
2025-06-24 20:38:11 +0100

    transferred generic signal quality assessment steps from ppg_quality to signal_templatequality

--- neurokit2/ppg/ppg_quality.py
+++ neurokit2/ppg/ppg_quality.py
@@ -2,14 +2,11 @@
 
 import numpy as np
 
-from ..epochs import epochs_to_df
-from ..signal import signal_interpolate
-from .ppg_peaks import ppg_peaks
-from .ppg_segment import ppg_segment
+from ..signal.signal_templatequality import signal_templatequality
 
 
 def ppg_quality(
-    ppg_cleaned, ppg_pw_peaks=None, sampling_rate=1000, method="templatematch", approach=None
+    ppg_cleaned, peaks=None, sampling_rate=1000, method="templatematch", approach=None
 ):
     """**PPG Signal Quality Assessment**
 
@@ -20,8 +17,7 @@ def ppg_quality(
       individual pulse wave and an average (template) pulse wave shape. This index is therefore
       relative: 1 corresponds to pulse waves that are closest to the average pulse wave shape (i.e.
       correlate exactly with it) and 0 corresponds to there being no correlation with the average
-      pulse wave shape. Note that 1 does not necessarily mean "good": use this index with care and
-      plot it alongside your PPG signal to see if it makes sense.
+      pulse wave shape.
 
     * The ``"disimilarity"`` method (loosely based on Sabeti et al., 2019) computes a continuous index
       of quality of the PPG signal, by calculating the level of disimilarity between each individual 
@@ -35,7 +31,7 @@ def ppg_quality(
     ----------
     ppg_cleaned : Union[list, np.array, pd.Series]
         The cleaned PPG signal in the form of a vector of values.
-    ppg_pw_peaks : tuple or list
+    peaks : tuple or list
         The list of PPG pulse wave peak samples returned by ``ppg_peaks()``. If None, peaks is computed from
         the signal input.
     sampling_rate : int
@@ -45,7 +41,7 @@ def ppg_quality(
 
     Returns
     -------
-    array
+    quality : array
         Vector containing the quality index ranging from 0 to 1 for ``"templatematch"`` method,
         or an unbounded value (where 0 indicates high quality) for ``"disimilarity"`` method.
 
@@ -79,118 +75,19 @@ def ppg_quality(
 
     method = method.lower()  # remove capitalised letters
 
+    # Detect PPG peaks (if not done already)
+    if peaks is None:
+        _, peaks = peaks(ppg_cleaned, sampling_rate=sampling_rate)
+        peaks = peaks["peaks"]
+
     # Run selected quality assessment method
     if method in ["templatematch"]:
-        quality = _ppg_quality_templatematch(
-            ppg_cleaned, ppg_pw_peaks=ppg_pw_peaks, sampling_rate=sampling_rate
+        quality = signal_templatequality(
+            ppg_cleaned, beat_inds=peaks, signal_type='ppg', sampling_rate=sampling_rate, method='templatematch'
         )
     elif method in ["disimilarity"]:
-        quality = _ppg_quality_disimilarity(
-            ppg_cleaned, ppg_pw_peaks=ppg_pw_peaks, sampling_rate=sampling_rate
+        quality = signal_templatequality(
+            ppg_cleaned, beat_inds=peaks, signal_type = 'ppg', sampling_rate=sampling_rate, method='disimilarity'
         )
-
-    return quality
-
-# =============================================================================
-# Calculate a template pulse wave
-# =============================================================================
-def _calc_template_pw(ppg_cleaned, ppg_pw_peaks=None, sampling_rate=1000):
-
-    # Sanitize inputs
-    if ppg_pw_peaks is None:
-        _, ppg_pw_peaks = ppg_peaks(ppg_cleaned, sampling_rate=sampling_rate)
-        ppg_pw_peaks = ppg_pw_peaks["PPG_Peaks"]
-
-    # Get heartbeats
-    heartbeats = ppg_segment(ppg_cleaned, ppg_pw_peaks, sampling_rate)
-    pw_data = epochs_to_df(heartbeats).pivot(
-        index="Label", columns="Time", values="Signal"
-    )
-    pw_data.index = pw_data.index.astype(int)
-    pw_data = pw_data.sort_index()
-
-    # Filter Nans
-    missing = pw_data.T.isnull().sum().values
-    nonmissing = np.where(missing == 0)[0]
-    pw_data = pw_data.iloc[nonmissing, :]
-
-    # Find template pulse wave
-    templ_pw = pw_data.mean()
-
-    return templ_pw, pw_data, ppg_pw_peaks
-
-
-# =============================================================================
-# Template-matching method
-# =============================================================================
-def _ppg_quality_templatematch(ppg_cleaned, ppg_pw_peaks=None, sampling_rate=1000):
-
-    # Obtain individual pulse waves and template pulse wave
-    templ_pw, pw_data, ppg_pw_peaks = _calc_template_pw(
-            ppg_cleaned, ppg_pw_peaks=ppg_pw_peaks, sampling_rate=sampling_rate
-        )
-    
-    # Find individual correlation coefficients (CCs)
-    cc = np.zeros(len(ppg_pw_peaks)-1)
-    for beat_no in range(0,len(ppg_pw_peaks)-1):
-        temp = np.corrcoef(pw_data.iloc[beat_no], templ_pw)
-        cc[beat_no] = temp[0,1]
-
-    # Interpolate beat-by-beat CCs
-    quality = signal_interpolate(
-        ppg_pw_peaks[0:-1], cc, x_new=np.arange(len(ppg_cleaned)), method="previous"
-    )
-
-    return quality
-
-# =============================================================================
-# Disimilarity measure method
-# =============================================================================
-def _norm_sum_one(pw):
-
-    # ensure all values are positive
-    pw = pw - pw.min() + 1
-
-    # normalise pulse wave to sum to one
-    pw = [x / sum(pw) for x in pw]
-
-    return pw
-
-def _calc_dis(pw1, pw2):
-    # following the methodology in https://doi.org/10.1016/j.imu.2019.100222 (Sec. 3.1.2.5)
-
-    # convert to numpy arrays
-    pw1 = np.array(pw1)
-    pw2 = np.array(pw2)
-
-    # normalise to sum to one
-    pw1 = _norm_sum_one(pw1)
-    pw2 = _norm_sum_one(pw2)
-    
-    # ignore any elements which are zero because log(0) is -inf
-    rel_els = (pw1 != 0) & (pw2 != 0)
-
-    # calculate disimilarity measure (using pw2 as the template)
-    dis = np.sum(pw2[rel_els] * np.log(pw2[rel_els] / pw1[rel_els]))
-
-    return dis
-
-
-def _ppg_quality_disimilarity(ppg_cleaned, ppg_pw_peaks=None, sampling_rate=1000):
-
-    # Obtain individual pulse waves and template pulse wave
-    templ_pw, pw_data, ppg_pw_peaks = _calc_template_pw(
-            ppg_cleaned, ppg_pw_peaks=ppg_pw_peaks, sampling_rate=sampling_rate
-        )
-    
-    # Find individual disimilarity measures
-    dis = np.zeros(len(ppg_pw_peaks)-1)
-    for beat_no in range(0,len(ppg_pw_peaks)-1):
-        dis[beat_no] = _calc_dis(pw_data.iloc[beat_no], templ_pw)
-
-    # Interpolate beat-by-beat dis's
-    quality = signal_interpolate(
-        ppg_pw_peaks[0:-1], dis, x_new=np.arange(len(ppg_cleaned)), method="previous"
-    )
 
     return quality
---
+++ neurokit2/signal/signal_templatequality.py
@@ -0,0 +1,171 @@
+# - * - coding: utf-8 - * -
+import numpy as np
+import pandas as pd
+
+from ..epochs import epochs_to_df
+from ..ppg.ppg_segment import ppg_segment
+from ..ecg.ecg_segment import ecg_segment
+from ..signal import signal_interpolate
+
+
+def signal_templatequality(signal, beat_inds, signal_type, sampling_rate=1000, method="templatematch"):
+    """**Assess quality of signal by comparing individual beat morphologies with a template**
+
+    Assess the quality of a signal (e.g. PPG or ECG) using the specified method. You can pass an unfiltered
+    signal as an input, but typically a filtered signal (e.g. cleaned using ``ppg_clean()`` or ``ecg_clean()``) will result in
+    more reliable results.
+
+
+    Parameters
+    ----------
+    signal : Union[list, np.array, pd.Series]
+        The cleaned signal, such as that returned by ``ppg_clean()`` or ``ecg_clean()``.
+    beat_inds : tuple or list
+        The list of beat samples (e.g. PPG or ECG peaks returned by ``ppg_peaks()`` or ``ecg_peaks()``).
+    signal_type : str
+        The signal type (e.g. 'ppg' or 'ecg').
+    sampling_rate : int
+        The sampling frequency of ``signal`` (in Hz, i.e., samples/second). Defaults to 1000.
+    method : str
+        The processing pipeline to apply. Can be one of ``"disimilarity"``, ``"templatematch"``. The default is
+        ``"templatematch"``.
+    **kwargs
+        Additional keyword arguments, usually specific for each method.
+
+    Returns
+    -------
+    quality : array
+        Vector containing the quality index ranging from 0 to 1 for ``"templatematch"`` method,
+        or an unbounded value (where 0 indicates high quality) for ``"disimilarity"`` method.
+
+    See Also
+    --------
+    ppg_quality
+
+    References
+    ----------
+    * Orphanidou, C. et al. (2015). "Signal-quality indices for the electrocardiogram and photoplethysmogram:
+      derivation and applications to wireless monitoring". IEEE Journal of Biomedical and Health Informatics, 19(3), 832-8.
+    * Sabeti E. et al. (2019). Signal quality measure for pulsatile physiological signals using morphological features: 
+      Applications in reliability measure for pulse oximetry. Informatics in Medicine Unlocked, 16, 100222.
+    """
+
+    signal_type = signal_type.lower()  # remove capitalised letters
+
+    # Run selected quality assessment method
+    if method in ["templatematch"]:  # Based on the approach in Orphanidou et al. (2015)
+        quality = _quality_templatematch(
+            signal, beat_inds=beat_inds, signal_type=signal_type, sampling_rate=sampling_rate,
+        )
+    elif method in ["disimilarity"]:  # Based on the approach in Sabeti et al. (2019)
+        quality = _quality_disimilarity(
+            signal, beat_inds=beat_inds, signal_type=signal_type, sampling_rate=sampling_rate
+        )
+
+    return quality
+
+# =============================================================================
+# Calculate template morphology
+# =============================================================================
+def _calc_template_morph(signal, beat_inds, signal_type, sampling_rate=1000):
+
+    # Segment to get individual beat morphologies
+    if signal_type == 'ppg':
+        heartbeats = ppg_segment(signal, beat_inds, sampling_rate)
+    elif signal_type == 'ecg':
+        heartbeats = ecg_segment(signal, beat_inds, sampling_rate)
+
+    # convert these to dataframe
+    ind_morph = epochs_to_df(heartbeats).pivot(
+        index="Label", columns="Time", values="Signal"
+    )
+    ind_morph.index = ind_morph.index.astype(int)
+    ind_morph = ind_morph.sort_index()
+
+    # Filter Nans
+    missing = ind_morph.T.isnull().sum().values
+    nonmissing = np.where(missing == 0)[0]
+    ind_morph = ind_morph.iloc[nonmissing, :]
+
+    # Find template pulse wave as the average pulse wave shape
+    templ_pw = ind_morph.mean()
+
+    return templ_pw, ind_morph, beat_inds
+
+
+# =============================================================================
+# Quality assessment using template-matching method
+# =============================================================================
+def _quality_templatematch(signal, beat_inds=None, signal_type='ppg', sampling_rate=1000):
+
+    # Obtain individual beat morphologies and template beat morphology
+    templ_morph, ind_morph, beat_inds = _calc_template_morph(
+            signal, beat_inds=beat_inds, signal_type=signal_type, sampling_rate=sampling_rate
+        )
+    
+    # Find correlation coefficients (CCs) between individual beat morphologies and the template
+    cc = np.zeros(len(beat_inds)-1)
+    for beat_no in range(0,len(beat_inds)-1):
+        temp = np.corrcoef(ind_morph.iloc[beat_no], templ_morph)
+        cc[beat_no] = temp[0,1]
+
+    # Interpolate beat-by-beat CCs
+    quality = signal_interpolate(
+        beat_inds[0:-1], cc, x_new=np.arange(len(signal)), method="previous"
+    )
+
+    return quality
+
+# =============================================================================
+# Disimilarity measure method
+# =============================================================================
+def _norm_sum_one(pw):
+
+    # ensure all values are positive
+    pw = pw - pw.min() + 1
+
+    # normalise pulse wave to sum to one
+    pw = [x / sum(pw) for x in pw]
+
+    return pw
+
+def _calc_dis(pw1, pw2):
+    # following the methodology in https://doi.org/10.1016/j.imu.2019.100222 (Sec. 3.1.2.5)
+
+    # convert to numpy arrays
+    pw1 = np.array(pw1)
+    pw2 = np.array(pw2)
+
+    # normalise to sum to one
+    pw1 = _norm_sum_one(pw1)
+    pw2 = _norm_sum_one(pw2)
+    
+    # ignore any elements which are zero because log(0) is -inf
+    rel_els = (pw1 != 0) & (pw2 != 0)
+
+    # calculate disimilarity measure (using pw2 as the template)
+    dis = np.sum(pw2[rel_els] * np.log(pw2[rel_els] / pw1[rel_els]))
+
+    return dis
+
+# =============================================================================
+# Quality assessment using disimilarity method
+# =============================================================================
+def _quality_disimilarity(signal, beat_inds=None, signal_type='ppg', sampling_rate=1000):
+
+    # Obtain individual beat morphologies and template beat morphology
+    templ_morph, ind_morph, beat_inds = _calc_template_morph(
+            signal, beat_inds=beat_inds, signal_type=signal_type, sampling_rate=sampling_rate
+        )
+    
+    # Find individual disimilarity measures
+    dis = np.zeros(len(beat_inds)-1)
+    for beat_no in range(0,len(beat_inds)-1):
+        dis[beat_no] = _calc_dis(ind_morph.iloc[beat_no], templ_morph)
+
+    # Interpolate beat-by-beat dis's
+    quality = signal_interpolate(
+        beat_inds[0:-1], dis, x_new=np.arange(len(signal)), method="previous"
+    )
+
+    return quality
